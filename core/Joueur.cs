61180657accfcf122fcd5d7add4e4fef913e8670
cverhulle <corentin.verhulle@gmail.com>
2025-06-27 17:20:15 +0200

    Création du constructeur de la classe.

--- core/Joueur.cs
+++ core/Joueur.cs
@@ -1,7 +1,13 @@
 public class Joueur
 {
     public string Nom { get; set; }
-    public int Vie { get; set; } = 100;
-    public int Force { get; set; } = 10;
+    public int Vie { get; set; }
+    public int Force { get; set; }
 
+    public Joueur(string nom)
+    {
+        Nom = nom;
+        Vie = 100;
+        Force = 10;
+    }
 }
