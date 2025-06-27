a562d081a46c111a8182b369f2a35665c6509802
cverhulle <corentin.verhulle@gmail.com>
2025-06-27 17:32:19 +0200

    On définit les propriétés get et set pour le nom.

--- core/Joueur.cs
+++ core/Joueur.cs
@@ -1,15 +1,28 @@
 public class Joueur
-{   
+{
     // La classe comporte trois attributs
-    public string nom { get; set; }
+    private string nom;
     private int vie;
     public int force { get; set; }
+    
+    // On définit les méthodes set et get pour le nom.
+    public string Nom
+    {
+        get => nom;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                nom = value;
+            else
+                Console.WriteLine("Le nom ne peut pas être vide ou null.");
+        }
+    }
 
     // On définit les méthodes set et get pour l'attribut vie.
     public int Vie
     {
         get => vie;
-        set => vie = Math.Max(0, value); 
+        set => vie = Math.Max(0, value);
     }
 
     // Constructeur de la classe ; La vie est réglée sur 100 et la force sur 10.
