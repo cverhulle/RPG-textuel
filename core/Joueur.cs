1f5cf5458895b6743de6a015e36e7e90b877eae7
cverhulle <corentin.verhulle@gmail.com>
2025-06-27 17:27:35 +0200

    On supprime l{get; set;}

--- core/Joueur.cs
+++ core/Joueur.cs
@@ -2,14 +2,14 @@ public class Joueur
 {   
     // La classe comporte trois attributs
     public string Nom { get; set; }
-    private int Vie { get; set; }
+    private int Vie;
     public int Force { get; set; }
 
     // On définit les méthodes set et get pour l'attribut vie.
     public int Vie
     {
-        get => vie;
-        set => vie = Math.Max(0, value); 
+        get => Vie;
+        set => Vie = Math.Max(0, value); 
     }
 
     // Constructeur de la classe ; La vie est réglée sur 100 et la force sur 10.
