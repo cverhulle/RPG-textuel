578eac353f77082b33c1650417136b2c4d8de39f
cverhulle <corentin.verhulle@gmail.com>
2025-06-27 17:21:17 +0200

    Ajout de commentaires.

--- core/Joueur.cs
+++ core/Joueur.cs
@@ -1,9 +1,11 @@
 public class Joueur
-{
+{   
+    // La classe comporte trois attributs
     public string Nom { get; set; }
     public int Vie { get; set; }
     public int Force { get; set; }
 
+    // Constructeur de la classe ; La vie est réglée sur 100 et la force sur 10.
     public Joueur(string nom)
     {
         Nom = nom;
