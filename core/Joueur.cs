b7dc7320beae9de197534bfcd26039f43ea1f8c0
cverhulle <corentin.verhulle@gmail.com>
2025-06-27 17:26:59 +0200

    On définit le get et set pour l'attribut vie.

--- core/Joueur.cs
+++ core/Joueur.cs
@@ -2,8 +2,15 @@ public class Joueur
 {   
     // La classe comporte trois attributs
     public string Nom { get; set; }
-    public int Vie { get; set; }
+    private int Vie { get; set; }
     public int Force { get; set; }
+
+    // On définit les méthodes set et get pour l'attribut vie.
+    public int Vie
+    {
+        get => vie;
+        set => vie = Math.Max(0, value); 
+    }
 
     // Constructeur de la classe ; La vie est réglée sur 100 et la force sur 10.
     public Joueur(string nom)
