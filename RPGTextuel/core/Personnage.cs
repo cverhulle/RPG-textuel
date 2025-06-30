c41d717e18b8a6fe102a79c60c39e814cc75a728
cverhulle <corentin.verhulle@gmail.com>
2025-06-30 10:26:34 +0200

    On crée une classe abstraite Personnage. Les classes Joueurs et ennemi en hériteront par la suite.

---
+++ RPGTextuel/core/Personnage.cs
@@ -0,0 +1,37 @@
+public abstract class Personnage
+{
+    // On définit les attributs de la classe Personnage.
+    private string nom;
+    private int vie;
+    private int force;
+
+    // On définit les méthodes set et get pour le nom.
+    // On rejette les noms "null" ou vide.
+    public string Nom
+    {
+        get => nom;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                nom = value;
+            else
+                Console.WriteLine("Le nom ne peut pas être vide ou null.");
+        }
+    }
+
+    // On définit les méthodes set et get pour l'attribut vie.
+    // On utilise Math pour mettre à 0 les points de vie s'ils tombent en négatif.
+    public int Vie
+    {
+        get => vie;
+        set => vie = Math.Max(0, value);
+    }
+
+    // On définit les méthodes set et get pour l'attribut force.
+    // On utilise Math pour mettre à 0 les points de force s'ils tombent en négatif.
+    public int Force
+    {
+        get => force;
+        set => force = Math.Max(0, value);
+    }
+}
