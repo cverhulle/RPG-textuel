d38caf5f2aae7b5e480fe7ae345a0d023bae6780
cverhulle <corentin.verhulle@gmail.com>
2025-06-30 14:36:59 +0200

    Implémentation de HealPotion

---
+++ RPGTextuel/Items/HealPotion.cs
@@ -0,0 +1,23 @@
+namespace RPGTextuel.Items
+{
+    // Création de l'objet Potion de soin
+    public class HealPotion : Item
+    {   
+        // On liste ses attributs
+        public int HealAmount { get; }
+
+        // On définiti son constructeur
+        public HealPotion(string name, int healAmount) : base(name)
+        {
+            HealAmount = healAmount;
+        }
+
+        // Cette méthode permet d'utiliser la potion de soin.
+        // Elle régénère un certain montant de PV.
+        public override void Use(Character target)
+        {
+            target.Heal(HealAmount);
+            Console.WriteLine($"{target.Name} utilise la {Name} et récupère {HealAmount} PV!");
+        }
+    }
+}
