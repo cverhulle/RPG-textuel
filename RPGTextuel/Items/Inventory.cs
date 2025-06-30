aab18ffdd75cb8f2ab87bf4dc6956da80b746c20
cverhulle <corentin.verhulle@gmail.com>
2025-06-30 16:14:47 +0200

    Fin de l'implémentation de inventory.

--- RPGTextuel/Items/Inventory.cs
+++ RPGTextuel/Items/Inventory.cs
@@ -8,11 +8,63 @@ namespace RPGTextuel
         // Cette classe contient une liste d'objet.
         private List<Item> items = new List<Item>();
 
-        // On crée un raccourci pour récupérer le nombre d'objets.
-        public int Count
+        // Cette méthode permet d'ajouter un item dans l'inventaire.
+        public void AddItem(Item item)
         {
-            get => items.Count;
+            items.Add(item);
+            Console.WriteLine($"Reçu {item.name}.");
         }
 
-    }    
+        // Cette méthode permet d'utiliser un item.
+        public void UseItem(int index, Character target)
+        {
+            if (IsValidItemIndex(index))
+            {
+                items[index].Use(target);
+                RemoveItem(index);
+            }
+            else
+            {
+                Console.WriteLine("Item introuvable");
+            }
+        }
+
+        // Cette méthode vérifie si l'index passé en argument est valide.
+        public bool IsValidItemIndex(int index)
+        {
+            return index >= 0 && index < items.Count;
+        }
+
+        // Cette méthode permet d'afficher les items du joueur
+        public void DisplayInventory(string ownerName)
+        {
+            // Si l'inventaire est vide, on affiche un message personnalisé
+            if (items.Count == 0)
+            {
+                Console.WriteLine($"{ownerName} n'a pas d'objet");
+                return;
+            }
+
+            // Sinon, on affiche la liste de sobjets du personnage.
+            Console.WriteLine($"\n Inventaire de {ownerName}");
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine($"  {i + 1}. {items[i].name}");
+            }
+        }
+
+        // Cette méthode permet de retirer un objet
+        public void RemoveItem(int index)
+        {
+            if (IsValidItemIndex(index))
+            {
+                items.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Erreur lors de la suppression de l'objet.");
+            }
+        }
+    }
 }
