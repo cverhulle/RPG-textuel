8a0fe2106d4c6d754e639f47f986403dca90b2fa
cverhulle <corentin.verhulle@gmail.com>
2025-06-30 14:40:47 +0200

    Ajout d'une liste d'item au joueur.

--- RPGTextuel/core/Player.cs
+++ RPGTextuel/core/Player.cs
@@ -1,5 +1,9 @@
+using RPGTextuel.Items;
 public class Player : Character
-{
+{   // On ajoute au joueur une liste d'item. 
+    // Par défaut, elle est vide.
+    public List<Item> Inventory { get; } = new List<Item>();
+
     // On définit un Player comme un Character à 100PV, 10 de force, 100PV max et dont le nom est laissé libre.
     public Player(string name) : base(name, 100, 10, 100)
     {
