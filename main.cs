aae1ca955fd9c1eba85eb5d1d04d9fc45fade238
cverhulle <corentin.verhulle@gmail.com>
2025-06-30 10:18:10 +0200

    Test du programme

--- main.cs
+++ main.cs
@@ -1,1 +1,16 @@
-// Création du projet
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Création du joueur et de l'ennemi
+        Joueur joueur = new Joueur("Link");
+        Ennemi ennemi = new Ennemi("Gobelin", 50, 8);
+
+        // Affichage initial
+        Console.WriteLine($"{joueur.Nom} entre dans l'arène !");
+        Console.WriteLine($"Un {ennemi.Nom} apparaît avec {ennemi.Vie} points de vie.\n");
+    }
+
+}
