2bb3ce860d2b9f1f330e8da443bd6de9f4a4b693
cverhulle <corentin.verhulle@gmail.com>
2025-06-30 14:57:44 +0200

    Implémentation de DamagePotion.

---
+++ RPGTextuel/Items/DamagePotion.cs
@@ -0,0 +1,23 @@
+namespace RPGTextuel.Items
+{   
+    // On crée la classe qui s'occupe de la potion de dégats.
+    public class DamagePotion : Item
+    {   
+        // On définit son attribut.
+        public int damage { get; }
+
+        // On implémente le constructeur de cette classe.
+        public DamagePotion(string name, int damagePotion) : base(name)
+        {
+            damage = damagePotion;
+        }
+
+        // On implémente la méthode Use.
+        // Elle prend en argument un personnage et lui enlève des PV égaux à damage.
+        public override void Use(Character target)
+        {
+            Console.WriteLine($"{target.Name} est touché par {Name}");
+            target.TakeDamage(damage);
+        }
+    }
+}
