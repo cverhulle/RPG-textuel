09104445b215f04090d1da162ee2e818c036ce75
cverhulle <corentin.verhulle@gmail.com>
2025-07-01 10:53:14 +0200

    On modifie la création de l'inventaire dans la classe Player.

--- RPGTextuel/core/Player.cs
+++ RPGTextuel/core/Player.cs
@@ -4,10 +4,11 @@ using RPGTextuel;
 public class Player : Character
 {   // On ajoute au joueur une liste d'item. 
     // Par défaut, elle est vide.
-    public Inventory Inventory { get; } = new Inventory();
+    public Inventory Inventory { get; }
 
     // On définit un Player comme un Character à 100PV, 10 de force, 100PV max et dont le nom est laissé libre.
     public Player(string name) : base(name, 100, 10, 100)
     {
+        Inventory = new Inventory(this);
     }
 }
