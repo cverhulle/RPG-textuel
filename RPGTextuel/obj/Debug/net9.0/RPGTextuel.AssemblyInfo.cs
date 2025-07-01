6dfe5af40734f9ad6707bb5c30ad0171b398ded1
cverhulle <corentin.verhulle@gmail.com>
2025-07-01 12:03:11 +0200

    On renomme le projet sur gitHub. Teste de la liaison.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e365365515c407d22a8a740baac20e9422e8b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6895a41bf1578730ce3c5e957f340fd812cda062")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
