eb3f868501c7245f10e93d1d4019751af3390ae8
cverhulle <corentin.verhulle@gmail.com>
2025-07-02 10:50:24 +0200

    On renomme SetupEnemies en SetupGoblins. Test du programme dans Program.cs

--- RPGTextuel/Enemies/SetupEnemies/SetupGoblins.cs
+++ RPGTextuel/Enemies/SetupEnemies/SetupGoblins.cs
@@ -1,7 +1,7 @@
 namespace RPGTextuel
 {
     // Cette classe permet de créer les ennemies que l'on affrontera
-    public static class SetupEnemies
+    public static class SetupGoblins
     {
         // Créeons la liste des ennemies de type "Goblin"
         public static List<Goblin> Goblins = new List<Goblin>
@@ -9,13 +9,6 @@ namespace RPGTextuel
             new Goblin("Snot", 36, 8, 40),
             new Goblin("Pak", 40, 7, 40),
             new Goblin("Tark", 45, 6, 45)
-        };
-
-        // Créeons la liste des ennemies de type "GoblinChief"
-        public static List<GoblinChief> GoblinChiefs = new List<GoblinChief>
-        {
-            new GoblinChief("Zonva", 60, 15, 60),
-            new GoblinChief("Grimlok", 65, 13, 65)
         };
     }
 }
--- RPGTextuel/Program.cs
+++ RPGTextuel/Program.cs
@@ -9,7 +9,7 @@ class Program
         Player joueur = new Player("Link");
 
         // On récupère un gobelin aléatoire
-        var chosenGoblin = SetupEnemies.GoblinChiefs.PickRandom();
+        var chosenGoblin = SetupGoblinChiefs.GoblinChiefs.PickRandom();
 
         Console.WriteLine($"Un gobelin nommé {chosenGoblin.Name} apparaît avec {chosenGoblin.Health} PV et {chosenGoblin.Strength} ATK !");
     }
