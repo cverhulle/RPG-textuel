74ca4dd7129e2691460a618ad859d901ed397ea5
cverhulle <corentin.verhulle@gmail.com>
2025-07-02 11:44:32 +0200

    Implémentation de SetupHealPotions

---
+++ RPGTextuel/Items/SetupHealPotions.cs
@@ -0,0 +1,10 @@
+namespace RPGTextuel.Items
+{
+    public static class SetupHealPotions
+    {
+        // Potions prédéfinies
+        public static readonly HealPotion SmallHealPoition = new HealPotion("Potion de soin (petite)", 30);
+        public static readonly HealPotion MediumHealPotion = new HealPotion("Potion de soin (moyennee)", 60);
+        public static readonly HealPotion LargeHealPotion = new HealPotion("Potion de soin (grande)", 100);
+    }
+}
