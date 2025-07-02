1ce9ca3b910a611ba4b28f4129abbac6de633e04
cverhulle <corentin.verhulle@gmail.com>
2025-07-02 11:55:23 +0200

    Création de HealPotionSize On regroupe les éléments liés aux potions de soin dans un dossier.

---
+++ RPGTextuel/Items/SetupItems.cs/HealPotions.cs/HealPotionSizeEnum.cs
@@ -0,0 +1,10 @@
+namespace RPGTextuel.Items.SetupItems.HealPotions
+{
+    // On crée une enum contenant les trois types de potions de soin possibles.
+    public enum HealPotionSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
--- RPGTextuel/Items/SetupItems.cs/HealPotions.cs/SetupHealPotions.cs
+++ RPGTextuel/Items/SetupItems.cs/HealPotions.cs/SetupHealPotions.cs
@@ -1,12 +1,12 @@
 using RPGTextuel.Items.Consumables;
 
-namespace RPGTextuel.Items.SetupItems
+namespace RPGTextuel.Items.SetupItems.HealPotions
 {
     public static class SetupHealPotions
     {
         // Potions prédéfinies
         public static readonly HealPotion SmallHealPoition = new HealPotion("Potion de soin (petite)", 30);
-        public static readonly HealPotion MediumHealPotion = new HealPotion("Potion de soin (moyennee)", 60);
+        public static readonly HealPotion MediumHealPotion = new HealPotion("Potion de soin (moyenne)", 60);
         public static readonly HealPotion LargeHealPotion = new HealPotion("Potion de soin (grande)", 100);
     }
 }
