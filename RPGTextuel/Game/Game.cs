3789332fa3f49e012ed338fbcc2446d73cf1fe70
cverhulle <corentin.verhulle@gmail.com>
2025-07-03 11:21:49 +0200

    Implémentation de HandlePlayerName

--- RPGTextuel/Game/Game.cs
+++ RPGTextuel/Game/Game.cs
@@ -12,31 +12,41 @@ namespace RPGTextuel.Game
             // On initialise une variable name de type string.
             string name;
 
-            // On itniatialise la variable pour savoir si le nom est valide.
+            // On initialise la variable pour savoir si le nom est valide.
             Boolean valid = false;
 
             do
-            {
+            {   
+                // On demande le nom à l'utilisateur.
                 Console.Write("🐱‍🏍 Entrez le nom de votre héros : ");
                 name = Console.ReadLine()?.Trim() ?? "";
 
-                // Si le nom est vide ou null, on affiche un message et, on repose la question.
-                if (GameUtils.IsNameNullOrWhiteSpace(name))
-                {
-                    Console.WriteLine("❌ Le nom ne peut pas être vide. Réessayez.\n");
-                }
-                else if (GameUtils.IsNameTooLong(name))
-                {
-                    Console.WriteLine($"❌ Le nom ne peut pas dépasser {GameConfig.MaxNameLength} Réessayez.\n");
-                }
-                else
-                {
-                    valid = true;
-                }
+                // On analyse si le nom est correct.
+                valid = HandlePlayerName(name);
 
             } while (valid);
 
             return name;
+        }
+
+        // Cette méthode prend un nom en argument et vérifie qu'il est valide pour un joueur.
+        private static Boolean HandlePlayerName(string name)
+        {
+            // Si le nom est vide ou null, on affiche un message et, on repose la question.
+            if (GameUtils.IsNameNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("❌ Le nom ne peut pas être vide. Réessayez.\n");
+                return false;
+            }
+            else if (GameUtils.IsNameTooLong(name))
+            {
+                Console.WriteLine($"❌ Le nom ne peut pas dépasser {GameConfig.MaxNameLength} Réessayez.\n");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
