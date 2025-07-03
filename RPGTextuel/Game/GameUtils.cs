5daa7c860476556be733d376220da7d82b825a30
cverhulle <corentin.verhulle@gmail.com>
2025-07-03 11:05:21 +0200

    Implémentation de IsNameNullOrWhiteSpace

--- RPGTextuel/Game/GameUtils.cs
+++ RPGTextuel/Game/GameUtils.cs
@@ -5,6 +5,19 @@ namespace RPGTextuel.Game
     // Cette classe s'occupe de gérer les méthodes utilitaires.
     public static class GameUtils
     {
+        // Cette méthode vérifie si un nom, donné en paramètre, est null ou vide.
+        public static Boolean IsNameNullOrWhiteSpace(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
         // Cette méthode vérifie si un nom, donné en paramètre, est trop long ou pas.
         public static Boolean IsNameTooLong(string name)
         {
