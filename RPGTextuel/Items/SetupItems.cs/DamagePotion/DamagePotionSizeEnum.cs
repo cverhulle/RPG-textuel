039c2fa8251c2ce03df1027ccc06092fc9b607b7
cverhulle <corentin.verhulle@gmail.com>
2025-07-03 14:36:22 +0200

    On crée un fichier pour gérer les potions de dégats.

---
+++ RPGTextuel/Items/SetupItems.cs/DamagePotion/DamagePotionSizeEnum.cs
@@ -0,0 +1,10 @@
+namespace RPGTextuel.Items.SetupItems.DamagePotions
+{
+    // On crée une enum contenant les trois types de potions de dégats possibles.
+    public enum DamagePotionSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
