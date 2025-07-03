6a617f1285ad701c5f1488c11ac1949d3b816aca
cverhulle <corentin.verhulle@gmail.com>
2025-07-03 10:18:03 +0200

    Création du fichier GameConfig.

---
+++ RPGTextuel/Config/GameConfig.cs
@@ -0,0 +1,14 @@
+namespace RPGTextuel.Config
+{
+    public static class GameConfig
+    {
+        // Taille de la barre de vie (nombre de cœurs)
+        public const int HealthBarLength = 10;
+
+        // Symbole pour un cœur rempli
+        public const string FilledHeartSymbol = "🧡 ";
+
+        // Symbole pour un cœur vide
+        public const string EmptyHeartSymbol = "🤍 ";
+    }
+}
