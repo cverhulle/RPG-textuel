2cdf9b9272a26ef57bf233ac05f5c4c56c89cd75
cverhulle <corentin.verhulle@gmail.com>
2025-07-03 10:44:48 +0200

    Implémentation de ShowWelcomeMessage

--- RPGTextuel/Game/Game.cs
+++ RPGTextuel/Game/Game.cs
@@ -1,8 +1,23 @@
 
 namespace RPGTextuel.Game
-{   
+{
     // Cette classe s'occupe de gérer la boucle de jeu.
     public static class GameLoop
     {
+
+        // Cette méthode affiche le message de début de jeu
+        private static void ShowWelcomeMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenue dans AventureRPG ✨");
+            Console.WriteLine("Vous allez affronter trois ennemis redoutables");
+            Console.WriteLine("Avant cela, donnez un nom à votre héros");
+        }
+
+        // Cette méthode récupère le nom du héros du joueur
+        private static void GetPlayerName()
+        {
+
+        }
     }
 }
