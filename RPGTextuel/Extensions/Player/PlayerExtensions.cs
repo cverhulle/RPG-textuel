abcf1b68fda78d25056f9209449fdb2740244f0e
cverhulle <corentin.verhulle@gmail.com>
2025-07-03 15:31:12 +0200

    Implémentation de AddDamagePotion

--- RPGTextuel/Extensions/Player/PlayerExtensions.cs
+++ RPGTextuel/Extensions/Player/PlayerExtensions.cs
@@ -1,17 +1,23 @@
 using RPGTextuel.Core;
 using RPGTextuel.Items.SetupItems;
 using RPGTextuel.Items.SetupItems.HealPotions;
-
+using RPGTextuel.Items.SetupItems.DamagePotions;
 
 namespace RPGTextuel.Extensions.Players
-{   
+{
     // Cette classe permet de définir les extensions de la classe Player
     public static class PlayerExtensions
     {
-        // Méthode d’extension pour ajouter une potion par sa taille.
-        public static void AddPotion(this Player player, PotionSize size)
+        // Méthode d’extension pour ajouter une potion de soin par sa taille.
+        public static void AddHealPotion(this Player player, PotionSize size)
         {
             SetupHealPotions.GiveHealPotionToPlayer(player, size);
+        }
+        
+        // Méthode d’extension pour ajouter une potion de dégats par sa taille.
+        public static void AddDamagePotion(this Player player, PotionSize size)
+        {
+            SetupDamagePotions.GiveDamagePotionToPlayer(player, size);
         }
     }
 }
