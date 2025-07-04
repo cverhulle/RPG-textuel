c49ffbb6775e3c357fc24fcd8761a2fd55111fbc
cverhulle <corentin.verhulle@gmail.com>
2025-07-04 14:48:30 +0200

    Début de l'implémentation de ShowMainMenu

--- RPGTextuel/Game/GameFeatures/GameDisplay.cs
+++ RPGTextuel/Game/GameFeatures/GameDisplay.cs
@@ -1,8 +1,32 @@
+using RPGTextuel.Core;
+
 namespace RPGTextuel.Game.GameFeatures
 {
     // Cette classe s'occupe de gérer l'affichage pour le jeu.
     public static class GameDisplay
     {
+
+        // Cette méthode affiche le menu principal du jeu
+        public static void ShowMainMenu(Player player)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("===== MENU PRINCIPAL =====");
+                Console.WriteLine("1. Passer au combat suivant.");
+                Console.WriteLine("2. Statistiques");
+                Console.WriteLine("3. Voir l'inventaire.");
+                Console.WriteLine("4. Quitter le jeu");
+                Console.WriteLine("==========================");
+                Console.Write("Choisissez une option : ");
+
+                string? input = Console.ReadLine();
+                Console.Clear();
+            }
+        }        
+
         // Cette méthode affiche le message de début de jeu
         public static void ShowWelcomeMessage()
         {
