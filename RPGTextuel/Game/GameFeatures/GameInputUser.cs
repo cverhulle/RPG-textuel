34a9b92df2151b3e85762e157250325a8c25da91
cverhulle <corentin.verhulle@gmail.com>
2025-07-04 15:19:06 +0200

    Fin de l'implémentation de AskMenuChoice

--- RPGTextuel/Game/GameFeatures/GameInputUser.cs
+++ RPGTextuel/Game/GameFeatures/GameInputUser.cs
@@ -6,14 +6,24 @@ namespace RPGTextuel.Game.GameFeatures
         // Cette méthode retourne le choix de l'utilisateur parmi un menu.
         public static int AskMenuChoice(List<string> options, string? title = null)
         {
+            // On affiche la liste des possibilités
+            GameDisplay.ShowOptionsList(options, title);
+
             while (true)
             {
-                // On affiche la liste des possibilités
-                GameDisplay.ShowOptionsList(options, title);
-
                 // On récupère la réponse de l'utilisateur.
                 Console.Write("\nFaites votre choix : ");
                 string? input = Console.ReadLine();
+
+                // On analyse la réponse de l'utilisateur
+                if (int.TryParse(input, out int choice) &&
+                        choice >= 1 && choice <= options.Count)
+                {
+                    return choice;
+                }
+
+                // Si l'entrée n'est pas valide, on affiche un message.
+                Console.WriteLine("Entrée invalide.");
             }
         }
     }
