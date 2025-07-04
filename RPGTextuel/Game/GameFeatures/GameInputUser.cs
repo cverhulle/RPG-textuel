0f6a478ee42d4a932d9ef1a643992c4ae82da3d7
cverhulle <corentin.verhulle@gmail.com>
2025-07-04 15:28:39 +0200

    Implémentation de AskUserInput

--- RPGTextuel/Game/GameFeatures/GameInputUser.cs
+++ RPGTextuel/Game/GameFeatures/GameInputUser.cs
@@ -3,6 +3,16 @@ namespace RPGTextuel.Game.GameFeatures
     // Cette regroupe les méthodes liées aux demandes envers l'utilisateur.
     public class GameInputUser
     {
+        // Cette méthode affiche une question à l'utilisateur et retourne sa réponse.
+        public static string AskUserInput(string prompt)
+        {
+            // On affiche la question à l'utilisateur.
+            Console.Write(prompt);
+
+            // On retourne la réponse et, par défaut un message vide.
+            return Console.ReadLine() ?? string.Empty;
+        }
+
         // Cette méthode retourne le choix de l'utilisateur parmi un menu.
         public static int AskMenuChoice(List<string> options, string? title = null)
         {
