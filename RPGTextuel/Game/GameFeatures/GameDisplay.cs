ab100c682324808c13bdc1b69b5cf6dc7bcae63e
cverhulle <corentin.verhulle@gmail.com>
2025-07-04 16:15:10 +0200

    On ajoute une ligne décorative à la fin de ShowOptionsList

--- RPGTextuel/Game/GameFeatures/GameDisplay.cs
+++ RPGTextuel/Game/GameFeatures/GameDisplay.cs
@@ -7,19 +7,30 @@ namespace RPGTextuel.Game.GameFeatures
         // Elle prend en paramètre une liste de phrases à afficher et, éventuellement, un titre.
         public static void ShowOptionsList(List<string> options, string? title = null)
         {
+            // Si options est vide ou null, on retourne une erreur.
             if (options == null || options.Count == 0)
             {
                 throw new ArgumentException("Le menu ne peut pas être vide.");
             }
 
+            // On affiche le titre si présent
             if (!string.IsNullOrWhiteSpace(title))
             {
                 Console.WriteLine($"===== {title.ToUpper()} =====\n");
             }
 
+            // On affiche la liste des options.
             for (int i = 0; i < options.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+
+            // On affiche une ligne décorative qui matche avec le titre.
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                int backlineLength = $"===== {title} =====".Length;
+                Console.WriteLine(new string('=', backlineLength));
+                Console.WriteLine();
             }
         }        
 
