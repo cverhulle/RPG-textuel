debb2a4604281bd79cf723ca19e2a8edeab0718c
cverhulle <corentin.verhulle@gmail.com>
2025-07-04 14:58:05 +0200

    On déplace la logique de ShowMainMenu dans HandleMainMenu

--- RPGTextuel/Game/GameFeatures/GameDisplay.cs
+++ RPGTextuel/Game/GameFeatures/GameDisplay.cs
@@ -9,52 +9,7 @@ namespace RPGTextuel.Game.GameFeatures
         // Cette méthode affiche le menu principal du jeu
         public static void ShowMainMenu(Player player)
         {
-            // On crée une variable pour gérer une boucle
-            bool isRunning = true;
-
-            // Tant qu'aucune réponse n'est fournie, on repète la question.
-            while (isRunning)
-            {
-
-                // On affiche le menu principal.
-                Console.Clear();
-                Console.WriteLine("===== MENU PRINCIPAL =====");
-                Console.WriteLine("1. Passer au combat suivant.");
-                Console.WriteLine("2. Statistiques");
-                Console.WriteLine("3. Voir l'inventaire.");
-                Console.WriteLine("4. Quitter le jeu");
-                Console.WriteLine("==========================");
-                Console.Write("Choisissez une option : ");
-
-                // On récupère la réponse de l'utilisateur.
-                string? input = Console.ReadLine();
-                Console.Clear();
-
-                // On évalue la réponse de l'utilisateur et, on lance la méthode associée.
-                switch (input)
-                {
-                    case "1":
-                        Console.WriteLine("Combat suivant ");                        
-                        break;
-
-                    case "2":
-                        Console.WriteLine("=== STATISTIQUES ===");
-                        break;
-
-                    case "3":
-                        Console.WriteLine("=== INVENTAIRE ===");
-                        break;
-
-                    case "4":
-                        Console.WriteLine("À bientôt !");
-                        isRunning = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Option invalide. Appuyez sur une touche pour réessayer...");
-                        break;
-                }
-            }
+            
         }        
 
         // Cette méthode affiche le message de début de jeu
