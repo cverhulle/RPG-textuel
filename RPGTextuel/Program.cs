a764f9eb902a3859a986ab0b15bf8350680383a0
cverhulle <corentin.verhulle@gmail.com>
2025-07-04 14:34:15 +0200

    Ajout de commentaires. Test du programme.

--- RPGTextuel/Program.cs
+++ RPGTextuel/Program.cs
@@ -5,6 +5,7 @@ class Program
     // Cette méthode permet de lancer le programme.
     static void Main(string[] args)
     {
+        // On appelle la méthode pour lancer le jeu.
         Game.StartGame();
     }
 }
