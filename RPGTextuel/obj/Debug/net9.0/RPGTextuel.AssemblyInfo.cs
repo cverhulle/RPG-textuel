07eafcbdb31d90377e171563968a6bd9a845cc03
cverhulle <corentin.verhulle@gmail.com>
2025-07-07 11:11:17 +0200

    On déplace le fichier du menu principal et celui pour fermer le jeu dans le dossier MainMenu

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32d410c85b6430fd5be4df024fc272a73da4404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1071cbb4d96f3755c0d8ae0e2e10492766bf8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
