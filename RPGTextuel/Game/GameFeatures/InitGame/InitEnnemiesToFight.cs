30ad5ff4b4e5ed1debe3f19671435b1147df6412
cverhulle <corentin.verhulle@gmail.com>
2025-07-08 15:16:04 +0200

    Création et commentaire de la classe InitEnnemiesToFight

---
+++ RPGTextuel/Game/GameFeatures/InitGame/InitEnnemiesToFight.cs
@@ -0,0 +1,10 @@
+namespace RPGTextuel.Game.GameFeatures.InitPlayer
+{
+    // Cette classe s'occupe de créer une liste contenant les ennemis que le jouer
+    // affrontera pendant cette partie.
+    public static class InitEnnemiesToFight
+    {
+
+        
+    }
+}
