81de8ba09292036c661466d06592388a63075fec
cverhulle <corentin.verhulle@gmail.com>
2025-07-09 15:05:27 +0200

    Début de l'implémentation de la fonction de ocmbat.

--- RPGTextuel/Game/GameFeatures/Fight/MainFight.cs
+++ RPGTextuel/Game/GameFeatures/Fight/MainFight.cs
@@ -1,6 +1,24 @@
+using RPGTextuel.Core;
+using RPGTextuel.Enemies.Class;
+
 namespace RPGTextuel.Game.GameFeatures.MainFight
 {
+    // Cette classe est la classe principale qui s'occupe des combats.
     public class MainFights
     {
+        // Cette classe s'occupe du déroulé du combat.
+        public static void HandleBattle(Player player, Enemy enemy)
+        {
+            // Tant que les 2 combattants sont en vie, le combat continue.
+            while (player.IsAlive && enemy.IsAlive)
+            {
+                // Affichage des infos de combat.
+                // Tour du joueur
+                // Tour de l'ennemi
+            }
+
+            // Résultat du combat. Retour de la fonction ?
+        }
+
     }
 }
