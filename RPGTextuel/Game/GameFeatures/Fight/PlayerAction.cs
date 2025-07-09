588e32df7ff3a393b6ae59af82dde6f452a1bd91
cverhulle <corentin.verhulle@gmail.com>
2025-07-09 15:41:02 +0200

    On crée une variable playersAction qui liste les actions possibles pour le joueur.

--- RPGTextuel/Game/GameFeatures/Fight/PlayerAction.cs
+++ RPGTextuel/Game/GameFeatures/Fight/PlayerAction.cs
@@ -6,9 +6,20 @@ namespace RPGTextuel.Game.GameFeatures.Fight
     // Cette méthode gère le tour d'action du joueur
     public static class PlayerAction
     {
+        // On liste les actions possibles pour un joueur pendant son tour.
+        private static readonly List<string> playersAction = new List<string>
+        {
+            "Attaquer",
+            "Utiliser un objet",
+            "Voir mes stats",
+            "Voir les stats de l'ennemi"
+        };
+
         // Cette méthode gère le tour du joueur.
         public static void PlayerTurn(Player player, Enemy enemy)
         {
+            Console.WriteLine("/n===== VOTRE TOUR =====");
+
             // On crée le booléen asActed pour savoir si le joueur a terminé son tour ou non.
             bool hasActed = false;
 
