5ec4a7a66fe050fce56df3f5dbfcf427dcbb10b2
cverhulle <corentin.verhulle@gmail.com>
2025-07-09 15:17:12 +0200

    Implémentation de ShowBattleState

--- RPGTextuel/Game/GameFeatures/Fight/BattleDisplay.cs
+++ RPGTextuel/Game/GameFeatures/Fight/BattleDisplay.cs
@@ -1,5 +1,6 @@
 using RPGTextuel.Core;
 using RPGTextuel.Enemies.Class;
+using RPGTextuel.Extensions.Characters;
 
 namespace RPGTextuel.Game.GameFeatures.Fight
 {
@@ -9,6 +10,18 @@ namespace RPGTextuel.Game.GameFeatures.Fight
         // Cette classe s'occupe d'afficher les infos sur les deux adversaires en combat.
         public static void ShowBattleState(Player player, Enemy enemy)
         {
+            Console.Clear();
+            Console.WriteLine("===== COMBAT EN COURS =====\n");
+
+            // Affichage de la santé du joueur
+            player.PrintHealthBar();
+
+            Console.WriteLine("\n--- Ennemi ---");
+
+            // Affichage de la santé de l'ennemi.
+            enemy.PrintHealthBar();
+
+            Console.WriteLine("\n============================\n");
         }
     }
 }
