9dbb18bf8f5486d478786c579b5998f9a6c4b004
cverhulle <corentin.verhulle@gmail.com>
2025-07-10 15:21:20 +0200

    On remet le lancement du jeu dans la  méthode Main.

--- RPGTextuel/Program.cs
+++ RPGTextuel/Program.cs
@@ -1,10 +1,5 @@
-﻿using RPGTextuel.Core;
+﻿
 using RPGTextuel.Game;
-using RPGTextuel.Enemies.Class;
-using RPGTextuel.SetupEnemies;
-using RPGTextuel.Game.GameFeatures.Fight;
-using RPGTextuel.Extensions.Players;
-using RPGTextuel.Items.SetupItems;
 
 class Program
 {
@@ -12,18 +7,6 @@ class Program
     static void Main(string[] args)
     {
         // Méthode pour lancer le jeu
-        // Game.StartGame();
-        
-        // Test d'un combat
-        Player player = new Player("Link");
-        player.AddHealPotion(PotionSize.Small);
-        player.AddDamagePotion(PotionSize.Small);
-        player.TakeDamage(20);
-
-        Enemy enemy = SetupGoblins.GetRandomGoblin();
-
-        MainFight.HandleBattle(player, enemy);    
-        
-
+        Game.StartGame();
     }
 }
