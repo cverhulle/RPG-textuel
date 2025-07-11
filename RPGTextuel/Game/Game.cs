19f54b845558a3517d9099f97b1b85a3f59c8002
cverhulle <corentin.verhulle@gmail.com>
2025-07-11 11:48:00 +0200

    Création d'un fichier InitGame contenant la méthode pour initialiser le jeu.

--- RPGTextuel/Game/Game.cs
+++ RPGTextuel/Game/Game.cs
@@ -26,30 +26,6 @@ namespace RPGTextuel.Game
             }
         }
 
-        // Cette méthode permet d'initialiser la partie.
-        // Elle affiche les messages d'accueils, demande le nom du joueur et l'instancie.
-        // Elle s'occupe également de créer la liste des ennemis à affronter pendant cette partie.
-        // Elle retourne le joueur ainsi crée de type Player.
-        private static (Player player, List<Enemy> ennemies) InitGame()
-        {
-            // On affiche le message d'accueil
-            InitWelcomeMessage.ShowWelcomeMessage();
-
-            // On récupère le nom du joueur
-            string name = InitPlayerName.AskAndVerifyPlayerName();
-
-            // On crée l'instance de type joueur
-            Player player = InitPlayerCreation.CreateAPlayer(name);
-
-            // On crée la liste des ennemis pendant cette partie
-            List<Enemy> ennemies = InitEnnemiesToFight.GetRandomGoblinSet();
-
-            // On affiche le dexième message d'accueil
-            InitWelcomeMessage.ShowWelcomeMessage2(name);
-
-            return (player, ennemies);
-        }
-
         // Cette méthode lance la boucle de jeu.
         // Elle retourne, en booléan, l'arrêt de la partie.
         private static Boolean LaunchGame(Player player, List<Enemy> ennemies)
---
+++ RPGTextuel/Game/GameFeatures/InitGame/InitGame.cs
@@ -0,0 +1,32 @@
+using RPGTextuel.Core;
+using RPGTextuel.Enemies.Class;
+
+namespace RPGTextuel.Game.GameFeatures.InitPlayer
+{
+    public static class InitGame
+    {
+        // Cette méthode permet d'initialiser la partie.
+        // Elle affiche les messages d'accueils, demande le nom du joueur et l'instancie.
+        // Elle s'occupe également de créer la liste des ennemis à affronter pendant cette partie.
+        // Elle retourne le joueur ainsi crée de type Player et la liste des ennemis.
+        public static (Player player, List<Enemy> ennemies) InitTheGame()
+        {
+            // On affiche le message d'accueil
+            InitWelcomeMessage.ShowWelcomeMessage();
+
+            // On récupère le nom du joueur
+            string name = InitPlayerName.AskAndVerifyPlayerName();
+
+            // On crée l'instance de type joueur
+            Player player = InitPlayerCreation.CreateAPlayer(name);
+
+            // On crée la liste des ennemis pendant cette partie
+            List<Enemy> ennemies = InitEnnemiesToFight.GetRandomGoblinSet();
+
+            // On affiche le dexième message d'accueil
+            InitWelcomeMessage.ShowWelcomeMessage2(name);
+
+            return (player, ennemies);
+        }
+    }
+}
