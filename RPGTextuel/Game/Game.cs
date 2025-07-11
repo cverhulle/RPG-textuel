b9a6c45b0fd13e498b09817a34c948f9b65858a2
cverhulle <corentin.verhulle@gmail.com>
2025-07-11 11:40:18 +0200

    Implémentation de LaunchGame()

--- RPGTextuel/Game/Game.cs
+++ RPGTextuel/Game/Game.cs
@@ -16,33 +16,6 @@ namespace RPGTextuel.Game
             // On initialise la partie
             (Player player, List<Enemy> ennemies) = InitGame();
 
-            // Pour chaque ennemi dans la liste
-            foreach (Enemy enemy in ennemies)
-            {
-                // On affiche un message indiquant qu'on passe au combat suivant.
-                Console.Clear();
-                Console.WriteLine($"Un nouvel ennemi approche !");
-                enemy.PrintStats();
-                GameUtils.WaitForUser();
-
-                // On délègue la gestion du menu principal à une méthode dédiée
-                Boolean wantsToQuit = FightingAnEnemyMenu.HandleMainMenu(player, enemy);
-
-                // Si le joueur quite, on arrête la partie
-                if (wantsToQuit)
-                {
-                    CloseGame.CloseTheGame();
-                    return;
-                }
-
-                // Si le joueur meurt, on arrête la partie
-                if (!player.IsAlive)
-                {
-                    Console.WriteLine("Le joueur est mort. Fin de la partie.");
-                    return;
-                }
-            }
-
             // Si tous les ennemis sont vaincus
             VictoryMessage();
         }
@@ -69,6 +42,37 @@ namespace RPGTextuel.Game
             InitWelcomeMessage.ShowWelcomeMessage2(name);
 
             return (player, ennemies);
+        }
+
+        // Cette méthode lance la boucle de jeu.
+        private static void LaunchGame(Player player, List<Enemy> ennemies)
+        {
+            // Pour chaque ennemi dans la liste
+            foreach (Enemy enemy in ennemies)
+            {
+                // On affiche un message indiquant qu'on passe au combat suivant.
+                Console.Clear();
+                Console.WriteLine($"Un nouvel ennemi approche !");
+                enemy.PrintStats();
+                GameUtils.WaitForUser();
+
+                // On délègue la gestion du menu principal à une méthode dédiée
+                Boolean wantsToQuit = FightingAnEnemyMenu.HandleMainMenu(player, enemy);
+
+                // Si le joueur quite, on arrête la partie
+                if (wantsToQuit)
+                {
+                    CloseGame.CloseTheGame();
+                    return;
+                }
+
+                // Si le joueur meurt, on arrête la partie
+                if (!player.IsAlive)
+                {
+                    Console.WriteLine("Le joueur est mort. Fin de la partie.");
+                    return;
+                }
+            }
         }
 
         // Cette méthode gère l'affichage en cas de victoire du joueur.
