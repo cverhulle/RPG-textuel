9eb57a25c96608543b20b67bdb55f8a711dd39cf
cverhulle <corentin.verhulle@gmail.com>
2025-07-11 15:53:39 +0200

    On appelle DisplayType pour afficher le type de l'ennemi. Test du jeu : tout est à jour.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6fd3176aae90a11e9df5aece3ea9be6785d9e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6b6c1abb41dcd884c103bb25405a04c18b0fb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
