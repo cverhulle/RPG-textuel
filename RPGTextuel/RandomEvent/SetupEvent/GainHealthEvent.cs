f0e0745bb103961cde0d3bf35dc1e957e11f8de4
cverhulle <corentin.verhulle@gmail.com>
2025-07-15 15:30:28 +0200

    Implémentation de GainHealthEvent

---
+++ RPGTextuel/RandomEvent/SetupEvent/GainHealthEvent.cs
@@ -0,0 +1,25 @@
+using RPGTextuel.Core;
+using RPGTextuel.RandomEvent.Class;
+
+namespace RPGTextuel.RandomEvent.SetupEvent
+{
+    // On définit l'évenement "Gain de Points de vie".
+    public class GainHealthEvent : RandomEventClass
+    {
+        // On définit son nom et sa description.
+        public override string Name => "Délicieux repas";
+        public override string Description => "Ce délicieux repas vous fait regagner des points de vie !";
+
+        // On définit son activation : 
+        // Gain aléatoire entre 10 et 30 PV.
+        public override void Trigger(Player player)
+        {
+            // On choisit aléatoirement le montant de PV gagné.
+            int healAmount = Random.Shared.Next(10, 30);
+
+            // Le joueur se soigne.
+            // Le message d'affichage est géré par la méthode Heal.
+            player.Heal(healAmount);
+        }
+    }
+}
--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89d97665b956590b3dd3489703e8a8f5a85ce2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a0dc7110cc69d5e80887cdac76a7d623f15246b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
