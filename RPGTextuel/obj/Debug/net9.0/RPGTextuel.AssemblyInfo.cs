3173ea056fce79ae810ddfc07e0d40de728ee0e3
cverhulle <corentin.verhulle@gmail.com>
2025-07-15 15:16:56 +0200

    Création d'un dossier et d'un fichier pour les événements aléatoires.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e71d88abf394e51763f776893770855bcb99ee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+954877aee6c1df30087c4d930c48b66adcd0e03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
