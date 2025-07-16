7f0f74b0663b2bd083d61bfeb2253d9191d777be
cverhulle <corentin.verhulle@gmail.com>
2025-07-16 14:29:34 +0200

    On ajoute le premier Test dans TestLauncher.

--- RPGTextuel/Test/TestLauncher.cs
+++ RPGTextuel/Test/TestLauncher.cs
@@ -1,3 +1,7 @@
+using RPGTextuel.Core;
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.Extensions.Enemies;
+
 namespace RPGTextuel.Test
 {
     // Cette classe lance le test.
@@ -5,7 +9,15 @@ namespace RPGTextuel.Test
     {
         public static void Main()
         {
-            
+            TestSetupPlayerAndGoblinAndSmallHealAndDamagePotion();
+        }
+
+        // Cette méthode permet de tester la méthode SetupPlayerAndGoblinAndSmallHealAndDamagePotion.
+        public static void TestSetupPlayerAndGoblinAndSmallHealAndDamagePotion()
+        {
+            (Player player, Enemy enemy) = TestUtils.SetupPlayerAndGoblinAndSmallHealAndDamagePotion();
+            player.Inventory.DisplayInventory();
+            enemy.PrintStats();
         }
     }
 }
