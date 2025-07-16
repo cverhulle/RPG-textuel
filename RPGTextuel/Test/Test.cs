22d4749613dfe0fad2e932a7d694d4c4eb14ce94
cverhulle <corentin.verhulle@gmail.com>
2025-07-16 11:24:02 +0200

    Implémentation de TestPlayerCreation

--- RPGTextuel/Test/Test.cs
+++ RPGTextuel/Test/Test.cs
@@ -1,4 +1,6 @@
 // Ce fichier permet de tester les différentes fonctionnalités du programme.
+using RPGTextuel.Core;
+
 namespace RPGTextuel.Test
 {
     public static class TestProgram
@@ -7,6 +9,12 @@ namespace RPGTextuel.Test
         public static void GameTest()
         {
 
+        }
+
+        // Cette fonction permet de créer un personnage nommé Link
+        public static Player TestPlayerCreation()
+        {
+            return new Player("Link");
         }
     }
 }
