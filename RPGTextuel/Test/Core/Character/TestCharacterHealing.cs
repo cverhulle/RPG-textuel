7b440ccd176c4876e3a0202b1956f5fafb07d691
cverhulle <corentin.verhulle@gmail.com>
2025-07-17 15:53:13 +0200

    Améliorations des tests : Plus d'affichage et analyse des résultats.

--- RPGTextuel/Test/Core/Character/TestCharacterHealing.cs
+++ RPGTextuel/Test/Core/Character/TestCharacterHealing.cs
@@ -1,4 +1,6 @@
 using RPGTextuel.Core;
+using RPGTextuel.Extensions.Characters;
+using RPGTextuel.Game.GameUtilsNamespace;
 using RPGTextuel.Test.Core;
 
 namespace RPGTextuel.Test.CoreCharacter
@@ -9,33 +11,61 @@ namespace RPGTextuel.Test.CoreCharacter
         // Cette méthode teste CanBeHealed avec une vie pleine
         public static void TestCanBeHealedWithFullHealth()
         {
+            Console.WriteLine("=== Test : CanBeHealed (vie pleine) ===");
+
             Player player = TestPlayerFactory.TestPlayerCreation();
-            Console.WriteLine("Le joueur peut-il être soigné ?");
-            Console.WriteLine(player.CanBeHealed(1));
+            player.PrintHealthBar();
+
+            bool result = player.CanBeHealed(1);
+            Console.WriteLine($"Résultat de CanBeHealed(1) : {result} (attendu : False)");
+
+            GameUtils.WaitForUser();
         }
 
         // Cette méthode teste CanBeHealed avec une vie non pleine
         public static void TestCanBeHealedNotWithFullHealth()
         {
+            Console.WriteLine("=== Test : CanBeHealed (vie non pleine) ===");
             Player player = TestPlayerFactory.TestPlayerCreation();
             player.TakeDamage(20);
-            Console.WriteLine("Le joueur peut-il être soigné ?");
-            Console.WriteLine(player.CanBeHealed(1));
+            player.PrintHealthBar();
+
+
+            bool result = player.CanBeHealed(1);
+            Console.WriteLine($"Résultat de CanBeHealed(1) : {result} (attendu : True)");
+
+            GameUtils.WaitForUser();
         }
 
         // Cette méthode teste Heal avec une vie pleine
         public static void TestHealWithFullHealth()
         {
+            Console.WriteLine("=== Test : Heal (avec vie pleine) ===");
             Player player = TestPlayerFactory.TestPlayerCreation();
+            player.PrintHealthBar();
+
             player.Heal(1);
+
+            player.PrintHealthBar(); // Pas de changement attendu.
+
+            GameUtils.WaitForUser();
         }
 
         // Cette méthode teste Heal avec une vie pleine non pleine
         public static void TestHealNotWithFullHealth()
         {
+            Console.WriteLine("=== Test : Heal (avec vie manquante) ===");
+
             Player player = TestPlayerFactory.TestPlayerCreation();
             player.TakeDamage(20);
-            player.Heal(1);
+            player.PrintHealthBar();
+
+            Console.WriteLine("Soins de 10 PV...");
+            player.Heal(10);
+
+            player.PrintHealthBar(); // Augmentation attendue
+
+            GameUtils.WaitForUser();
         }
     }
 }
