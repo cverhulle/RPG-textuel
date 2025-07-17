1bb3869242f818929812b0f55482098105c0cbf7
cverhulle <corentin.verhulle@gmail.com>
2025-07-17 14:41:18 +0200

    Test de TakeDamage.

--- RPGTextuel/Test/Core/TestCharacterCombat.cs
+++ RPGTextuel/Test/Core/TestCharacterCombat.cs
@@ -1,8 +1,11 @@
+using RPGTextuel.Core.Characters;
+
 namespace RPGTextuel.Test.Core
 {
     // Cette classe permet de tester les méthodes de combat de la classe Character
     public static class TestCharacterCombat
     {
+        // On teste la méthode TakeDamage.
         public static void TestTakeDamage(Character character)
         {
             character.TakeDamage(20);
