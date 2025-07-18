2b55d6e26d0f3dff9a49ac9bb4e263aac5768c05
cverhulle <corentin.verhulle@gmail.com>
2025-07-18 11:24:32 +0200

    Ajout des tests sur les potions moyennes et grandes.

--- RPGTextuel/Test/Items/TestItems.cs
+++ RPGTextuel/Test/Items/TestItems.cs
@@ -33,5 +33,61 @@ namespace RPGTextuel.Test.Items
             Console.WriteLine("Inventaire après ajout de la potion de dégâts :");
             player.Inventory.DisplayInventory();
         }
+
+        // Vérifie qu'une potion de soin "moyenne" est bien ajoutée à l'inventaire.
+        public static void TestAddMediumHealPotion()
+        {
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+
+            Console.WriteLine("Inventaire avant ajout de la potion de soin :");
+            player.Inventory.DisplayInventory();
+
+            player.AddMediumHealPotionToInventory();
+
+            Console.WriteLine("Inventaire après ajout de la potion de soin :");
+            player.Inventory.DisplayInventory();
+        }
+
+        // Vérifie qu'une potion de dégâts "moyenne" est bien ajoutée à l'inventaire.
+        public static void TestAddMediumDamagePotion()
+        {
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+
+            Console.WriteLine("Inventaire avant ajout de la potion de soin :");
+            player.Inventory.DisplayInventory();
+
+            player.AddMediumDamagePotionToInventory();
+
+            Console.WriteLine("Inventaire après ajout de la potion de dégâts :");
+            player.Inventory.DisplayInventory();
+        }
+
+        // Vérifie qu'une potion de soin "grande" est bien ajoutée à l'inventaire.
+        public static void TestAddLargeHealPotion()
+        {
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+
+            Console.WriteLine("Inventaire avant ajout de la potion de soin :");
+            player.Inventory.DisplayInventory();
+
+            player.AddLargeHealPotionToInventory();
+
+            Console.WriteLine("Inventaire après ajout de la potion de soin :");
+            player.Inventory.DisplayInventory();
+        }
+
+        // Vérifie qu'une potion de dégâts "grande" est bien ajoutée à l'inventaire.
+        public static void TestAddLargeDamagePotion()
+        {
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+
+            Console.WriteLine("Inventaire avant ajout de la potion de soin :");
+            player.Inventory.DisplayInventory();
+
+            player.AddLargeDamagePotionToInventory();
+
+            Console.WriteLine("Inventaire après ajout de la potion de dégâts :");
+            player.Inventory.DisplayInventory();
+        }
     }
 }
