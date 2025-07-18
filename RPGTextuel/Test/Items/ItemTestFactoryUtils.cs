a368a83673a2f690830148d5a2356c67ab54440e
cverhulle <corentin.verhulle@gmail.com>
2025-07-18 11:22:19 +0200

    On ajoute, dans la Factory, l'ajout de potion de taille medium et large.

--- RPGTextuel/Test/Items/ItemTestFactoryUtils.cs
+++ RPGTextuel/Test/Items/ItemTestFactoryUtils.cs
@@ -18,5 +18,29 @@ namespace RPGTextuel.Test.Items
         {
             player.AddDamagePotion(PotionSize.Small);
         }
+
+        // Cette fonction permet d'ajouter une potion de soin de taille moyenne dans l'inventaire
+        public static void AddMediumHealPotionToInventory(this Player player)
+        {
+            player.AddHealPotion(PotionSize.Medium);
+        }
+
+        // Cette fonction permet d'ajouter une potion de soin de taille moyenne dans l'inventaire
+        public static void AddMediumDamagePotionToInventory(this Player player)
+        {
+            player.AddDamagePotion(PotionSize.Medium);
+        }
+
+        // Cette fonction permet d'ajouter une potion de soin de taille "grande" dans l'inventaire
+        public static void AddLargeHealPotionToInventory(this Player player)
+        {
+            player.AddHealPotion(PotionSize.Large);
+        }
+
+        // Cette fonction permet d'ajouter une potion de soin de taille "grande" dans l'inventaire
+        public static void AddLargeDamagePotionToInventory(this Player player)
+        {
+            player.AddDamagePotion(PotionSize.Large);
+        }
     }
 }
