34b17166107c0b70f4107968b6038ad0a11b8520
cverhulle <corentin.verhulle@gmail.com>
2025-07-18 10:54:06 +0200

    On renomme le fichier PlayerTestFactory. On ajoute trois types de joueurs possibles.

--- RPGTextuel/Test/Core/PlayerTestFactory.cs
+++ RPGTextuel/Test/Core/PlayerTestFactory.cs
@@ -4,21 +4,29 @@ using RPGTextuel.Test.Items;
 namespace RPGTextuel.Test.Core
 {
     // Cette classe regroupe les tests liés à la classe Player.
-    public static class TestPlayerFactory
+    public static class PlayerTestFactory
     {
         // Cette fonction permet de créer un personnage nommé Link
-        public static Player TestPlayerCreation()
+        public static Player CreateBasicTestPlayer()
         {
             return new Player("Link");
         }
 
         // Cette fonction permet de créer le joueur.
         // On ajoute une potion de dégats et une de soin au joueur
-        public static Player SetupPlayerAndSmallHealAndDamagePotion()
+        public static Player CreatePlayerWithHealAndDamagePotions()
         {
-            Player player = TestPlayerCreation();
-            player.TestAddingSmallDamagePotion();
+            Player player = CreateBasicTestPlayer();
             player.TestAddingSmallHealPotion();
+            player.TestAddingSmallDamagePotion();
+            return player;
+        }
+
+        // Joueur blessé
+        public static Player CreateWoundedPlayer(int damage)
+        {
+            Player player = CreateBasicTestPlayer();
+            player.TakeDamage(damage);
             return player;
         }
     }
