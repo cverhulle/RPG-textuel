35c41411c1fd4a4f59e818df081c8296ab0981c7
cverhulle <corentin.verhulle@gmail.com>
2025-07-18 11:10:49 +0200

    Mise à jour du fichier : On teste PrintStats pour les 3 types d'ennemis.

--- RPGTextuel/Test/Extensions/Enemy/TestenemyExtensions.cs
+++ RPGTextuel/Test/Extensions/Enemy/TestenemyExtensions.cs
@@ -1,5 +1,6 @@
 using RPGTextuel.Enemies.Class;
 using RPGTextuel.Extensions.Enemies;
+using RPGTextuel.Game.GameUtilsNamespace;
 using RPGTextuel.Test.Enemies;
 
 namespace RPGTextuel.Test.ExtensionsEnemy
@@ -7,11 +8,31 @@ namespace RPGTextuel.Test.ExtensionsEnemy
     // Cette classe teste les méthodes du dossier Enemy dans Extensions
     public static class TestEnemyExtensions
     {
-        // Cette méthode teste la méthode PrintStats
-        public static void TestPrintStats()
+        // On teste la méthode PrintStats pour un petit gobelin.
+        public static void TestGoblinMinionStats()
         {
-            Enemy enemy = TestEnememyFactory.TestGoblinMinionEnemyCreation();
+            Console.WriteLine("=== Test : PrintStats (Goblin Minion) ===");
+            Enemy enemy = EnemyTestFactory.CreateGoblinMinion();
             enemy.PrintStats();
+            GameUtils.WaitForUser();
+        }
+
+        // On teste la méthode PrintStats pour un gobelin.
+        public static void TestGoblinStats()
+        {
+            Console.WriteLine("=== Test : PrintStats (Goblin) ===");
+            Enemy enemy = EnemyTestFactory.CreateGoblin();
+            enemy.PrintStats();
+            GameUtils.WaitForUser();
+        }
+
+        // On teste la méthode PrintStats pour un chef gobelin.
+        public static void TestGoblinChiefStats()
+        {
+            Console.WriteLine("=== Test : PrintStats (Goblin Chief) ===");
+            Enemy enemy = EnemyTestFactory.CreateGoblinChief();
+            enemy.PrintStats();
+            GameUtils.WaitForUser();
         }
     }
 }
