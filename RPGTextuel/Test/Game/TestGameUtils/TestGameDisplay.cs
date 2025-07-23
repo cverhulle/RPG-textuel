bbea4d147dd8ffa679bf45df8e80b2b1e88417be
cverhulle <corentin.verhulle@gmail.com>
2025-07-23 14:31:17 +0200

    Implémentation de TestPrintStatsWithPlayer et de TestPrintStatsWithEnemy

--- RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
+++ RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
@@ -1,6 +1,8 @@
 using RPGTextuel.Core;
+using RPGTextuel.Enemies.Class;
 using RPGTextuel.Game.GameUtilsNamespace;
 using RPGTextuel.Test.Core;
+using RPGTextuel.Test.Enemies;
 
 namespace RPGTextuel.Test.Game.TestGameUtils
 {
@@ -15,7 +17,7 @@ namespace RPGTextuel.Test.Game.TestGameUtils
             List<string> elements = new List<string> { };
 
             Console.WriteLine("Résultat attendu : Message informant que la liste est vide \n");
-            
+
             Console.WriteLine("=== Résultat obtenu ===");
             GameDisplay.PrintFramedList(elements, item => item, "Objets du joueur", showCount: true);
         }
@@ -63,7 +65,7 @@ namespace RPGTextuel.Test.Game.TestGameUtils
             List<string> elements = new List<string> { "Potion de soin", "Potion de dégâts" };
 
             Console.WriteLine("Résultat attendu : \n  ===== OBJETS DU JOUEUR ===== \n 1. Potion de soin \n 2. Potion de dégâts \n ============= \n 2 élément(s) au total. \n ");
-            
+
             Console.WriteLine("=== Résultat obtenu ===");
             GameDisplay.PrintFramedList(elements, item => item, "Objets du joueur", showCount: true);
 
@@ -85,6 +87,30 @@ namespace RPGTextuel.Test.Game.TestGameUtils
 
             Console.WriteLine("=== Résultat obtenu ===");
             GameDisplay.PrintInventory(player);
+        }
+
+        // Test de PrintStats avec un player.
+        public static void TestPrintStatsWithPlayer()
+        {
+            Console.WriteLine("=== Test de PrintStatsWithPlayer ===");
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+
+            Console.WriteLine("\nRésultat attendu :\n- Affichage des statistiques du joueur\n- Puis message : Appuyez sur une touche pour continuer...\n");
+
+            Console.WriteLine("=== Résultat obtenu ===");
+            GameDisplay.PrintStats(player);
+        }
+        
+        // Test de PrintStats avec un ennemi.
+        public static void TestPrintStatsWithEnemy()
+        {
+            Console.WriteLine("=== Test de PrintStatsWithEnemy ===");
+            Enemy enemy = EnemyTestFactory.CreateGoblin();
+
+            Console.WriteLine("\nRésultat attendu :\n- Affichage des statistiques de l'ennemi\n- Puis message : Appuyez sur une touche pour continuer...\n");
+
+            Console.WriteLine("=== Résultat obtenu ===");
+            GameDisplay.PrintStats(enemy);
         }
     }
 }
