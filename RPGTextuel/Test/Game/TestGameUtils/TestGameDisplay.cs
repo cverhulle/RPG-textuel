b6f132d6b45d03ed7390ca1db3f56d261466d449
cverhulle <corentin.verhulle@gmail.com>
2025-07-23 14:15:51 +0200

    Implémentation de TestPrintFramedListWithEmptyList

--- RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
+++ RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
@@ -1,8 +1,36 @@
+using RPGTextuel.Game.GameUtilsNamespace;
+
 namespace RPGTextuel.Test.Game.TestGameUtils
 {
     // Cette classe permet de tester GameDisplay
     public static class TestGameDisplay
     {
+
+        // Test de la méthode PrintFramedList avec une liste vide 
+        public static void TestPrintFramedListWithEmptyList()
+        {
+            Console.WriteLine("=== Test de PrintFramedListWithEmptyList ===");
+            List<string> elements = new List<string> {};
+
+            Console.WriteLine("Résultat attendu : Message informant que la liste est vide \n Résultat obtenu :");
+            GameDisplay.PrintFramedList(elements, item => item, "Objets du joueur", showCount: true);
+        }
+
+        // Test de la méthode PrintFramedList
+        public static void TestPrintFramedList()
+        {
+            Console.WriteLine("=== Test de PrintFramedList ===");
+
+            List<string> elements = new List<string> { "Potion de soin", "Potion de dégâts" };
+            GameDisplay.PrintFramedList(elements, item => item, "Objets du joueur", showCount: true);
+
+            // À l'écran, on doit voir :
+            // ===== OBJETS DU JOUEUR =====
+            //   1. Potion de soin
+            //   2. Potion de dégâts
+            // ===== Obj... =====
+            // 2 élément(s) au total.
+        }
     }
 
 }
--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f5807bb40477479fb193f8806ce2f0ff801d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4c1443dd4c4cce7261dc319a03c024334b8ae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
