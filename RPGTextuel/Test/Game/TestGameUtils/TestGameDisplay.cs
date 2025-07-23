9e0e26e4cfcda0278839f85c592d626edb18f310
cverhulle <corentin.verhulle@gmail.com>
2025-07-23 14:24:45 +0200

    Implémentation de TestPrintFramedListWithAllParams

--- RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
+++ RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
@@ -31,7 +31,7 @@ namespace RPGTextuel.Test.Game.TestGameUtils
             //   2. Potion de dégâts
             // 2 élément(s) au total.
         }
-        
+
         // Test de la méthode PrintFramedList sans showcount
         public static void TestPrintFramedListWithoutShowcount()
         {
@@ -47,6 +47,24 @@ namespace RPGTextuel.Test.Game.TestGameUtils
             //   1. Potion de soin
             //   2. Potion de dégâts
             // ============================
+        }
+
+        // Test de la méthode PrintFramedList avec tous les paramètres
+        public static void TestPrintFramedListWithAllParams()
+        {
+            Console.WriteLine("=== Test de PrintFramedListWithAllParams ===");
+
+            List<string> elements = new List<string> { "Potion de soin", "Potion de dégâts" };
+
+            Console.WriteLine("Résultat attendu : \n  ===== OBJETS DU JOUEUR ===== \n 1. Potion de soin \n 2. Potion de dégâts \n ============= \n 2 élément(s) au total. \n Résultat obtenu : ");
+            GameDisplay.PrintFramedList(elements, item => item, "Objets du joueur", showCount: true);
+
+            // À l'écran, tu devrais voir :
+            // ===== OBJETS DU JOUEUR =====
+            //   1. Potion de soin
+            //   2. Potion de dégâts
+            // ============================
+            // 2 élément(s) au total.
         }
     }
 
