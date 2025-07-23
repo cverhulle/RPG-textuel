d9019decd4ef9b83ebbf577baa7f6d0462ae24d4
cverhulle <corentin.verhulle@gmail.com>
2025-07-23 14:21:01 +0200

    Implémentation de TestPrintFramedListWithoutShowcount

--- RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
+++ RPGTextuel/Test/Game/TestGameUtils/TestGameDisplay.cs
@@ -10,7 +10,7 @@ namespace RPGTextuel.Test.Game.TestGameUtils
         public static void TestPrintFramedListWithEmptyList()
         {
             Console.WriteLine("=== Test de PrintFramedListWithEmptyList ===");
-            List<string> elements = new List<string> {};
+            List<string> elements = new List<string> { };
 
             Console.WriteLine("Résultat attendu : Message informant que la liste est vide \n Résultat obtenu :");
             GameDisplay.PrintFramedList(elements, item => item, "Objets du joueur", showCount: true);
@@ -30,6 +30,23 @@ namespace RPGTextuel.Test.Game.TestGameUtils
             //   1. Potion de soin
             //   2. Potion de dégâts
             // 2 élément(s) au total.
+        }
+        
+        // Test de la méthode PrintFramedList sans showcount
+        public static void TestPrintFramedListWithoutShowcount()
+        {
+            Console.WriteLine("=== Test de PrintFramedListWithoutShowcount ===");
+            List<string> elements = new List<string> { "Potion de soin", "Potion de dégâts" };
+
+            Console.WriteLine("Résultat attendu : \n  ===== OBJETS DU JOUEUR ===== \n 1. Potion de soin \n 2. Potion de dégâts \n ============= \n Résultat obtenu : ");
+
+            GameDisplay.PrintFramedList(elements, item => item, showCount: true);
+
+            // À l'écran, tu devrais voir :
+            // ===== OBJETS DU JOUEUR =====
+            //   1. Potion de soin
+            //   2. Potion de dégâts
+            // ============================
         }
     }
 
