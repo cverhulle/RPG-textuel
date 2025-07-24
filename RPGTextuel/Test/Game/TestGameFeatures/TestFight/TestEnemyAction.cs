5c5156376d7f2b440c0b64fc4222865e645a5fc7
cverhulle <corentin.verhulle@gmail.com>
2025-07-24 10:29:54 +0200

    Implémentation de TestEnemyTurn

---
+++ RPGTextuel/Test/Game/TestGameFeatures/TestFight/TestEnemyAction.cs
@@ -0,0 +1,28 @@
+using RPGTextuel.Core;
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.Game.GameFeatures.Fight;
+using RPGTextuel.Test.Core;
+using RPGTextuel.Test.Enemies;
+
+namespace RPGTextuel.Test.Game.TestGameFeatures
+{
+    // Cette classe permet de teset EnemyAction
+    public static class TestEnemyAction
+    {
+        // Cette méthode permet de tester EnemyTurn
+        public static void TestEnemyTurn()
+        {
+            Console.WriteLine("=== Test EnemyTurn ===");
+
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+            Enemy enemy = EnemyTestFactory.CreateGoblin();
+
+            Console.WriteLine($"Vie du joueur avant l'attaque : {player.Health}");
+
+            // On simule l'attaque de l'ennemi
+            EnemyAction.EnemyTurn(player, enemy);
+
+            Console.WriteLine($"Vie du joueur après l'attaque : {player.Health} (attendu : {enemy.Attack} dégats)");
+        }
+    }
+}
