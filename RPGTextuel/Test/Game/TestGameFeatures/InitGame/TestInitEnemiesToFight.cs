0f6d21adf84d2bff60f1cd6be0ad0f7ac75f778d
cverhulle <corentin.verhulle@gmail.com>
2025-07-25 10:17:33 +0200

    Implémentation de TestGetRandomGoblinSet

--- RPGTextuel/Test/Game/TestGameFeatures/InitGame/TestInitEnemiesToFight.cs
+++ RPGTextuel/Test/Game/TestGameFeatures/InitGame/TestInitEnemiesToFight.cs
@@ -1,3 +1,6 @@
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.Game.GameFeatures.InitGame;
+
 namespace RPGTextuel.Test.Game.TestGameFeatures
 {
     // Cette classe permet de tester InitEnemiesToFight
@@ -7,7 +10,21 @@ namespace RPGTextuel.Test.Game.TestGameFeatures
         // Test de la méthode GetRandomGoblinSet 
         public static void TestGetRandomGoblinSet()
         {
-            
+            Console.WriteLine("=== Test de GetRandomGoblinSet (Affichage) ===");
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"\n--- Tirage {i} ---");
+                List<Enemy> goblins = InitEnnemiesToFight.GetRandomGoblinSet();
+
+                for (int j = 0; j < goblins.Count; j++)
+                {
+                    Enemy goblin = goblins[j];
+                    Console.WriteLine($"Gobelin {j + 1} : Nom = {goblin.Name}, Type = {goblin.GetType().Name}");
+                }
+            }
+
+            Console.WriteLine("\nTest terminé.");
         }
     }
 }
