0df54c871202da5f7c52cba69fb0b9a3718ba4ec
cverhulle <corentin.verhulle@gmail.com>
2025-07-31 16:06:17 +0200

    Mise à jour de allEventFactoriesByDefault

--- RPGTextuel/RandomEvent/Factory/RandomEventFactory.cs
+++ RPGTextuel/RandomEvent/Factory/RandomEventFactory.cs
@@ -1,3 +1,4 @@
+using RPGTextuel.Config;
 using RPGTextuel.Extensions.List;
 using RPGTextuel.Items.Class;
 using RPGTextuel.RandomEvent.Class;
@@ -9,22 +10,21 @@ namespace RPGTextuel.RandomEvent.Factory
     // Cette classe permet de regrouper tous les évenements aléatoires.
     public static class RandomEventFactory
     {
-        // Liste de tous les événements disponibles.
-        private static readonly List<RandomEventClass> allEvents = new()
-        {
-            // -------------- TODO : Gérer l'événement FindItemEvent() -------------------
-            // new FindItemEvent(),
-            new GainHealthEvent(),
-            new LoseHealthEvent(),
-            new LoseItemEvent()
-        };
-
         // Cette méthode permet de créer un item pour trouver un objet.
         // En entrée, on donne la liste des objets et leur poids.
         public static RandomEventClass CreateFindItemEvent(List<Weighted<Item>> items)
         {
             return new FindItemEvent(items);
         }
+
+        // Liste de tous les événements disponibles.
+        private static readonly List<Func<RandomEventClass>> allEventFactoriesByDefault = new()
+        {
+            () => CreateFindItemEvent(ItemDropConfig.DefaultPotionDrops),
+            () => new GainHealthEvent(),
+            () => new LoseHealthEvent(),
+            () => new LoseItemEvent()
+        };
 
         // On choisit aléatoirement un événement parmi une liste.
         public static RandomEventClass GetRandomEvent(List<RandomEventClass> options)
