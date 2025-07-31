eb24c27e43daa0dcf2776b7d14d3a560583d48a0
cverhulle <corentin.verhulle@gmail.com>
2025-07-31 16:14:35 +0200

    Implémentation de ItemDropTableType

---
+++ RPGTextuel/Config/Enum/ItemDropTable.cs
@@ -0,0 +1,9 @@
+namespace RPGTextuel.Config.Enum
+{
+    // Cette enum contient les différents types de loot possibles.
+    public enum ItemDropTableType
+    {
+        Default,
+        Boss
+    }
+}
