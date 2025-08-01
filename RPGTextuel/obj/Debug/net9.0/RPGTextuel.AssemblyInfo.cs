27d4d05fb16874a85840ed2e51722a774401f914
cverhulle <corentin.verhulle@gmail.com>
2025-08-01 14:23:13 +0200

    On lance TestFindItemEventWithBossItemDrop : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5c21343d5dd8ce9f6a1c4e8eecfd3be451392c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5add291e104b1f0771dbd1e19c416b68d1e471f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
