71befb992f4a1bbc375c698e34c55d90e9c5c5bf
cverhulle <corentin.verhulle@gmail.com>
2025-08-04 10:29:48 +0200

    Suppression de la méthode FightingAnEnemyMenuUtils ; Test du programme : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd93c11d930f80b847f0cfabce6dccdbe5c13b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ae5a7629fd037fe4119b3485bf7c22cb1d28a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
