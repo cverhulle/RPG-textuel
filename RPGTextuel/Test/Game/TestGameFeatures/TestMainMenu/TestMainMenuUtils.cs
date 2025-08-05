853171a2630f3933747e0552b37f7e72a6aa26f8
cverhulle <corentin.verhulle@gmail.com>
2025-08-05 11:43:23 +0200

    Implémentation de TestShouldEndGameWhenPlayerIsDead

--- RPGTextuel/Test/Game/TestGameFeatures/TestMainMenu/TestMainMenuUtils.cs
+++ RPGTextuel/Test/Game/TestGameFeatures/TestMainMenu/TestMainMenuUtils.cs
@@ -18,5 +18,17 @@ namespace RPGTextuel.Test.Game.TestGameFeatures
 
             Console.WriteLine("Résultat attendu : true ; Résultat obtenu ${result}");
         }
+
+        // Cette méthode permet de tester ShouldEndGame quand le joueur est mort
+        public static void TestShouldEndGameWhenPlayerIsDead()
+        {
+            Console.WriteLine("=== Test de ShouldEndGameWhenPlayerIsDead ===");
+            Player player = PlayerTestFactory.CreateDeadPlayer();
+            bool wantsToQuit = false;
+
+            bool result = MainMenuUtils.ShouldEndGame(wantsToQuit, player);
+
+            Console.WriteLine("Résultat attendu : true ; Résultat obtenu ${result}");
+        }
     }
 }
