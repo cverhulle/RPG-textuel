ac1f277f39d1e81c9f168a75bc4e1c4e68f39c6c
cverhulle <corentin.verhulle@gmail.com>
2025-08-05 10:54:06 +0200

    On ajoute isFinalBoss dans l'appel de la méthode.

--- RPGTextuel/Game/GameFeatures/MainMenu/MainMenu.cs
+++ RPGTextuel/Game/GameFeatures/MainMenu/MainMenu.cs
@@ -22,7 +22,7 @@ namespace RPGTextuel.Game.GameFeatures.MainMenu
             foreach (Enemy enemy in ennemies)
             {
                 // Cette méthode s'occupe de gérer les événements entre les combats
-                MainMenuUtils.HandleEventsBetweenFights(player, enemy, fightNumber, "Un nouvel ennemi approche");
+                MainMenuUtils.HandleEventsBetweenFights(player, enemy, fightNumber, "Un nouvel ennemi approche", isFinalBoss);
 
                 // On délègue la gestion du menu principal à une méthode dédiée
                 Boolean wantsToQuit = FightingAnEnemyMenu.HandleMainMenu(player, enemy);
