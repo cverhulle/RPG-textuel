9b242f5dbe83eff53d553694c4f8a50ad90f33b3
cverhulle <corentin.verhulle@gmail.com>
2025-08-05 11:44:38 +0200

    Implémentation de TestShouldEndGameWhenGameShouldContinue

--- RPGTextuel/Test/Game/TestGameFeatures/TestMainMenu/TestMainMenuUtils.cs
+++ RPGTextuel/Test/Game/TestGameFeatures/TestMainMenu/TestMainMenuUtils.cs
@@ -7,16 +7,16 @@ namespace RPGTextuel.Test.Game.TestGameFeatures
     // Cette classe permet de tester MainMenuUtils
     public static class TestMainMenuUtils
     {
-        // Cette méthode permet de tester ShouldEndGame quand le joueur veut quitter
-        public static void TestShouldEndGameWhenPlayerWantsToQuit()
+        // Cette méthode permet de tester ShouldEndGame quand le jeu doit continuer
+        public static void TestShouldEndGameWhenGameShouldContinue()
         {
-            Console.WriteLine("=== Test de ShouldEndGameWhenPlayerWantsToQuit ===");
+            Console.WriteLine("=== Test de ShouldEndGameWhenGameShouldContinue ===");
             Player player = PlayerTestFactory.CreateBasicTestPlayer();
-            bool wantsToQuit = true;
+            bool wantsToQuit = false;
 
             bool result = MainMenuUtils.ShouldEndGame(wantsToQuit, player);
 
-            Console.WriteLine("Résultat attendu : true ; Résultat obtenu ${result}");
+            Console.WriteLine("Résultat attendu : false ; Résultat obtenu ${result}");
         }
 
         // Cette méthode permet de tester ShouldEndGame quand le joueur est mort
@@ -25,6 +25,18 @@ namespace RPGTextuel.Test.Game.TestGameFeatures
             Console.WriteLine("=== Test de ShouldEndGameWhenPlayerIsDead ===");
             Player player = PlayerTestFactory.CreateDeadPlayer();
             bool wantsToQuit = false;
+
+            bool result = MainMenuUtils.ShouldEndGame(wantsToQuit, player);
+
+            Console.WriteLine("Résultat attendu : true ; Résultat obtenu ${result}");
+        }
+
+        // Cette méthode permet de tester ShouldEndGame quand le joueur veut quitter
+        public static void TestShouldEndGameWhenPlayerWantsToQuit()
+        {
+            Console.WriteLine("=== Test de ShouldEndGameWhenPlayerWantsToQuit ===");
+            Player player = PlayerTestFactory.CreateBasicTestPlayer();
+            bool wantsToQuit = true;
 
             bool result = MainMenuUtils.ShouldEndGame(wantsToQuit, player);
 
