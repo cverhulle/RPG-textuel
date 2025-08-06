4d504ac802b7c0b845c2a84573bb476362793f73
cverhulle <corentin.verhulle@gmail.com>
2025-08-06 15:24:39 +0200

    On modifie les chances de coups critiques du joueur à 15%

--- RPGTextuel/core/Player.cs
+++ RPGTextuel/core/Player.cs
@@ -8,8 +8,8 @@ namespace RPGTextuel.Core
         // Par défaut, elle est vide.
         public Inventory Inventory { get; }
 
-        // On définit un Player comme un Character à 100PV, 10 de force, 100PV max et dont le nom est laissé libre.
-        public Player(string name) : base(name, 100, 10, 100)
+        // On définit un Player comme un Character à 100PV, 10 de force, 100PV max, 15% de chances de critique et dont le nom est laissé libre.
+        public Player(string name) : base(name, 100, 10, 100, 0.15)
         {
             Inventory = new Inventory(this);
         }
