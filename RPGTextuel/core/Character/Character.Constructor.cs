d78237dc4ed4941589e9a11e65843d4b457a87dd
cverhulle <corentin.verhulle@gmail.com>
2025-08-06 15:23:20 +0200

    ON ajoute criticalHitChance dans le constructeur.

--- RPGTextuel/core/Character/Character.Constructor.cs
+++ RPGTextuel/core/Character/Character.Constructor.cs
@@ -3,12 +3,13 @@ namespace RPGTextuel.Core.Characters
     public abstract partial class Character
     {
         // On implémente le constructeur de la classe.
-        protected Character(string characterName, int characterHealth, int characterStrength, int characterMaxHealth)
+        protected Character(string characterName, int characterHealth, int characterStrength, int characterMaxHealth, double criticalHitChance = 0.05)
         {
             name = characterName;
             Health = characterHealth;
             strength = characterStrength;
             MaxHealth = characterMaxHealth;
+            CriticalHitChance = criticalHitChance;
         }
 
         // Création de la propriété isAlive pour savoir si un personnage est en vie ou mort.
