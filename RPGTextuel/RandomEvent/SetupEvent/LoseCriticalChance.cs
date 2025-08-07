bc10c30bb2c2c4ddeae7c5ade2ffad55c2556bcf
cverhulle <corentin.verhulle@gmail.com>
2025-08-07 10:55:35 +0200

    Ajout de commentaires.

--- RPGTextuel/RandomEvent/SetupEvent/LoseCriticalChance.cs
+++ RPGTextuel/RandomEvent/SetupEvent/LoseCriticalChance.cs
@@ -3,12 +3,14 @@ using RPGTextuel.RandomEvent.Class;
 
 namespace RPGTextuel.RandomEvent.SetupEvent
 {
-    
+    // On définit l'événement "Baisse de la chance de coup critique"
     public class LoseCriticalChanceEvent : RandomEventClass
     {
+        // On définit le nom et la description
         public override string Name => "Vision troublée";
         public override string Description => "Une migraine soudaine trouble votre perception... Vos chances de viser juste diminuent.";
 
+        // Cet événement diminue la chance la coup critique du joueur.
         public override void Trigger(Player player)
         {
             // Réduction entre 0.03 (3%) et 0.1 (10%)
