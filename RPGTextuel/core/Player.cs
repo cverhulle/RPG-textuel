0241eed5806144d76a9443ebf0607ede12d15a28
cverhulle <corentin.verhulle@gmail.com>
2025-08-07 10:31:56 +0200

    On fixe le critique du joueur avec le paramètre de Gameconfig.

--- RPGTextuel/core/Player.cs
+++ RPGTextuel/core/Player.cs
@@ -1,3 +1,4 @@
+using RPGTextuel.Config;
 using RPGTextuel.Core.Characters;
 using RPGTextuel.Items.Class;
 
@@ -8,8 +9,11 @@ namespace RPGTextuel.Core
         // Par défaut, elle est vide.
         public Inventory Inventory { get; }
 
-        // On définit un Player comme un Character à 100PV, 10 de force, 100PV max, 15% de chances de critique et dont le nom est laissé libre.
-        public Player(string name) : base(name, 100, 10, 100, 0.15)
+        // On définit un Player comme un Character à 100PV, 10 de force, 100PV max.
+        // La chance de critique est fixé dans GameConfig 
+        // Le nom est laissé libre.
+        public Player(string name) :
+            base(name, 100, 10, 100, GameConfig.criticalHitPlayer)
         {
             Inventory = new Inventory(this);
         }
