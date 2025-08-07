5acd514b39179437ba85b58ec95fb4087100c85f
cverhulle <corentin.verhulle@gmail.com>
2025-08-07 10:51:49 +0200

    Implémentation du trigger

--- RPGTextuel/RandomEvent/SetupEvent/GainCriticalChance.cs
+++ RPGTextuel/RandomEvent/SetupEvent/GainCriticalChance.cs
@@ -10,9 +10,18 @@ namespace RPGTextuel.RandomEvent.SetupEvent
         public override string Name => "Eclair de génie !";
         public override string Description => "Un éclair de lucidité traverse votre esprit. Vos prochaines attaques seront plus précises !";
 
-        // On définit l'effet de l'événement.
+        // On définit l'effet de l'événement : Augmentation de la chance de coup critique.
         public override void Trigger(Player player)
         {
+            // On définit le boost entre 0.05 (5%) et 0.15 (15%)
+            double boost = Random.Shared.NextDouble() * 0.1 + 0.05;
+
+            // On applique le boost à la chance de coup critique du joueur
+            player.CriticalHitChance += boost;
+
+            // On affiche un message d'information.
+            Console.WriteLine($"Votre chance de coup critique a augmenté de {boost * 100:0.#}% !");
+            Console.WriteLine($"Chance de critique actuelle : {player.CriticalHitChance * 100:0.#}%");
         }
     }
 }
