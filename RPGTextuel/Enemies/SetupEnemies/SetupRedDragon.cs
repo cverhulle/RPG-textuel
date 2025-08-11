3b2701cbdc1a954eb962636ad83327461c5f26a6
cverhulle <corentin.verhulle@gmail.com>
2025-08-11 16:04:17 +0200

    Ajustement des commentaires.

--- RPGTextuel/Enemies/SetupEnemies/SetupRedDragon.cs
+++ RPGTextuel/Enemies/SetupEnemies/SetupRedDragon.cs
@@ -6,11 +6,11 @@ namespace RPGTextuel.SetupEnemies
     // Cette classe permet de créer les ennemies de type Dragon Rouge que l'on affrontera
     public static class SetupRedDragons
     {
-        // Créeons la liste des ennemies de type "Goblin"
+        // Créeons la liste des ennemies de type "Dragon Rouge"
         public static List<RedDragon> RedDragons = new List<RedDragon>
         {
             new RedDragon("Spyro", 100, 18, 100),
-            new RedDragon("Ignir", 70, 15, 70, 0.5),
+            new RedDragon("Ignir", 70, 15, 70, 0.5),   // moins de PV mais beaucoup de critique
         };
 
         // Cette méthode permet de choisir aléatoirement un élement de la liste des Dragons rouges.
