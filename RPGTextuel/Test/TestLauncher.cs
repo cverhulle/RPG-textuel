1df0115c7c209d95544176c1efcdfbe4adff8d4c
cverhulle <corentin.verhulle@gmail.com>
2025-08-12 11:47:37 +0200

    Test des 3 méthodes : OK

--- RPGTextuel/Test/TestLauncher.cs
+++ RPGTextuel/Test/TestLauncher.cs
@@ -8,7 +8,7 @@ namespace RPGTextuel.Test
         // On lance cette méthode pour tester le programme.
         public static void TestLauncherMain()
         {
-            RandomEventsTests.TestGainCriticalChancePlayerEventWithFullCrit();
+            RandomEventsTests.TestLoseCriticalChancePlayerEventWithAlmostNoCrit();
         }
     }
 }
--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88553fff39810026c7f6a44f277cf184d09fed30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c246d520faf6c25851f585b0945c3d8bb9e5bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
