8f5c736b34bc90f11cc8fdcbf0a1544b94b2cbf4
cverhulle <corentin.verhulle@gmail.com>
2025-08-12 11:37:47 +0200

    Ajout de commentaires.

--- RPGTextuel/RandomEvent/SetupEvent/LoseCriticalChancePlayer.cs
+++ RPGTextuel/RandomEvent/SetupEvent/LoseCriticalChancePlayer.cs
@@ -20,10 +20,10 @@ namespace RPGTextuel.RandomEvent.SetupEvent
                     0.0, 1.0,                                                   // La chance de coup critique varie entre 0% et 100%
                     0.03, 0.10,                                                 // On diminue la chance de coup critique entre 3% et 10%
                     false,                                                      // On veut réaliser une diminution
-                    "Votre précision est déjà au plus bas...",
-                    "Votre chance de coup critique a diminué de {0:0.#}% !",
-                    "Chance de critique actuelle : {0:0.#}%"
-                );
+                    "Votre précision est déjà au plus bas...",                  // Si la chance de coup critique est déjà à 0%; on personnalise le message
+                    "Votre chance de coup critique a diminué de {0:0.#}% !",    // Affichage pour l'utilisateur
+                    "Chance de critique actuelle : {0:0.#}%"                    // Affichage pour l'utilisateur
+                );  
         }
     }
 }
