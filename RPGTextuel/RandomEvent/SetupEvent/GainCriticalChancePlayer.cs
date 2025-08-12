34e5564a86aae1153705cb1be6ccd3e496a97645
cverhulle <corentin.verhulle@gmail.com>
2025-08-12 11:32:48 +0200

    Ajout de commentaires.

--- RPGTextuel/RandomEvent/SetupEvent/GainCriticalChancePlayer.cs
+++ RPGTextuel/RandomEvent/SetupEvent/GainCriticalChancePlayer.cs
@@ -16,13 +16,13 @@ namespace RPGTextuel.RandomEvent.SetupEvent
         {
             player.CriticalHitChance = PlayerStatHelper.ModifyStatWithBounds
                 (
-                    player.CriticalHitChance,
-                    0.0, 1.0,
-                    0.05, 0.15,
-                    true,
-                    "Votre précision est déjà parfaite ! Aucun gain possible.",
-                    "Votre chance de coup critique a augmenté de {0:0.#}% !",
-                    "Chance de critique actuelle : {0:0.#}%"
+                    player.CriticalHitChance,                                       // Stat à modifier (chance de coup critique)
+                    0.0, 1.0,                                                       // La chance de coup critique varie entre 0% et 100%
+                    0.05, 0.15,                                                     // On augmente la chance de coup critique entre 5% et 15%
+                    true,                                                           // On veut réaliser une augmentation
+                    "Votre précision est déjà parfaite ! Aucun gain possible.",     // Si la chance de coup critique est déjà à 100%; on personnalise le message
+                    "Votre chance de coup critique a augmenté de {0:0.#}% !",       // Affichage pour l'utilisateur
+                    "Chance de critique actuelle : {0:0.#}%"                        // Affichage pour l'utilisateur
                 );
         }
     }
