60c0696aba9ee21cb9b76c293527f67ef5fdae44
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 15:01:49 +0200

    Déplacement des méthodes de fin de jeu vers EndGame.

--- RPGTextuel/Game/GameFeatures/MainMenu/MainMenuUtils.cs
+++ RPGTextuel/Game/GameFeatures/MainMenu/MainMenuUtils.cs
@@ -62,23 +62,5 @@ namespace RPGTextuel.Game.GameFeatures.MainMenu
                 GameUtils.WaitForUser(color : ConsoleColor.Blue);
             }
         }
-
-        // Méthode utilitaire permettant de savoir si l'on doit arreter la partie.
-        // On retourne True si c'est le cas et, false sinon.
-        public static bool ShouldEndGame(bool wantsToQuit, Player player)
-        {
-            if (wantsToQuit)
-            {
-                CloseGame.CloseTheGame();
-                return true;
-            }
-
-            if (!player.IsAlive)
-            {
-                Console.WriteLine("Vous êtes mort. Fin de la partie.");
-                return true;
-            }
-            return false;
-        }
     }
 }
