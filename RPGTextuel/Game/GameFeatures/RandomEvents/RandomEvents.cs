aab19d68f121531b64ac05205af23a413e98198b
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 11:17:53 +0200

    On affiche les éléments relatifs aux objets en bleus.

--- RPGTextuel/Game/GameFeatures/RandomEvents/RandomEvents.cs
+++ RPGTextuel/Game/GameFeatures/RandomEvents/RandomEvents.cs
@@ -10,7 +10,9 @@ namespace RPGTextuel.Game.GameFeatures.RandomEvents
         // Cependant, il a déjà utilisé sa chance pour ce combat.
         public static void TriggerRandomEventButAlreadyTried()
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Vous avez déjà tenté votre chance pour cet affrontement !");
+            Console.ResetColor();
         }
 
         // Cette méthode permet de déclencher un événement aléatoire.
@@ -18,10 +20,15 @@ namespace RPGTextuel.Game.GameFeatures.RandomEvents
         public static void TriggerRandomEventWithIntro(Player player, string introMessage)
         {
             Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(introMessage);
+
             var randomEvent = RandomEventFactory.GetRandomEvent();
             Console.WriteLine($"\nÉvénement : {randomEvent.Name}");
             Console.WriteLine($"{randomEvent.Description}\n");
+            Console.ResetColor();
+
             randomEvent.Trigger(player);
         }
     }
