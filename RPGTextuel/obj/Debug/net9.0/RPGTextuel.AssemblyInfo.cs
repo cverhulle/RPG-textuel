f100a3e3433ce83df7d9ca5248c6b393086d6526
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 15:11:56 +0200

    Test : OK Double message quand on perd.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a805d77bbfa3aa7ff6d6c0322cf57840fa2e44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c6464a417bfaaa27f61f18a612120257a4b3534")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
