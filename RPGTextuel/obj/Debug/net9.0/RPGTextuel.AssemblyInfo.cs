1fe689c0b5b7e0c3e04b4b115c8150bb66822695
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 11:51:15 +0200

    Test des couleurs des events, objets et boss : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6835caa8181f7fcafa2e33713a64259531d74047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb1a86aaea7c00af7e733abfbae1475f0561af6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
