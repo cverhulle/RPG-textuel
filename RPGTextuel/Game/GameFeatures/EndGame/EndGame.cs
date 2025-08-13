031ccb483166356421d99d9f6d2588d7f98c83a8
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 14:58:15 +0200

    On déplace les méthodes de fin de partie dans EndGame.

--- RPGTextuel/Game/GameFeatures/EndGame/EndGame.cs
+++ RPGTextuel/Game/GameFeatures/EndGame/EndGame.cs
@@ -1,3 +1,4 @@
+using RPGTextuel.Core;
 using RPGTextuel.Game.GameUtilsNamespace;
 using RPGTextuel.Utils;
 
@@ -6,6 +7,31 @@ namespace RPGTextuel.Game.GameFeatures.EndGame
     // Cette classe s'occupe de gérer la fin de partie
     public static class EndGame
     {
+        // Cette méthode est appelée pour fermer le jeu.
+        public static Boolean CloseTheGame()
+        {
+            Console.WriteLine("À bientôt !");
+            return false;
+        }
+        
+        // Méthode utilitaire permettant de savoir si l'on doit arreter la partie.
+        // On retourne True si c'est le cas et, false sinon.
+        public static bool ShouldEndGame(bool wantsToQuit, Player player)
+        {
+            if (wantsToQuit)
+            {
+                CloseGame.CloseTheGame();
+                return true;
+            }
+
+            if (!player.IsAlive)
+            {
+                Console.WriteLine("Vous êtes mort. Fin de la partie.");
+                return true;
+            }
+            return false;
+        }
+
         // Cette méthode gère l'affichage en cas de victoire du joueur.
         public static void VictoryMessage()
         {
