c295996c6eb5b97607ea1cd5971f5bbd11e8ddb0
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 10:50:02 +0200

    Mise à jour de la TODO list.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9853a7b8a97e4ec59c102adf0672ab6acbed2fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c211d235434eff3882f05d855949d7c92acdc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
