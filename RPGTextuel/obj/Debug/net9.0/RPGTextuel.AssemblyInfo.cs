849a1b00862d2958a4f83935aecb7dc7be92a0c8
cverhulle <corentin.verhulle@gmail.com>
2025-08-13 15:41:36 +0200

    Test du jeu et des couleurs : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04fa9e6418b15f960239a040208ec735098dae6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890f94ee3eea84700586bba9d196b4e21e6e1b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
