b90e61bbca3f649f6be9723c9f7b15c3980f52a8
cverhulle <corentin.verhulle@gmail.com>
2025-08-14 12:05:25 +0200

    Personnalisation de la couleur dans AskUserInpt.

--- RPGTextuel/Game/GameUtils/GameInputUser.cs
+++ RPGTextuel/Game/GameUtils/GameInputUser.cs
@@ -1,23 +1,26 @@
+using RPGTextuel.Config;
+using RPGTextuel.Utils;
+
 namespace RPGTextuel.Game.GameUtilsNamespace
 {
     // Cette regroupe les méthodes liées aux demandes envers l'utilisateur.
     public class GameInputUser
     {
         // Cette méthode affiche une question à l'utilisateur et retourne sa réponse.
-        public static string AskUserInput(string prompt)
+        public static string AskUserInput(string prompt, ConsoleColor color = GameConfig.defaultColor)
         {
             // On affiche la question à l'utilisateur.
-            Console.Write(prompt);
+            DisplayUtils.WriteLineInColor(prompt, color);
 
             // On retourne la réponse et, par défaut un message vide.
             return Console.ReadLine() ?? string.Empty;
         }
 
         // Cette méthode retourne le choix de l'utilisateur parmi un menu.
-        public static int AskMenuChoice(List<string> options, string? title = null)
+        public static int AskMenuChoice(List<string> options, string? title = null, ConsoleColor color = GameConfig.defaultColor)
         {
             // On affiche la liste des possibilités
-            GameDisplay.ShowOptionsList(options, title);
+            GameDisplay.ShowOptionsList(options, title, color);
 
             while (true)
             {
