0855ead220f2981410d2630853ac740ea6bf85d4
cverhulle <corentin.verhulle@gmail.com>
2025-08-14 10:54:49 +0200

    Test du jeu : 1 WaitForUser à corriger.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a100a6e9338b06f69141b03c4d3f72bae70423")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbbc32d4c60333ecb728eeb45536fdc1a074b82")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
