046b13d381ff34be2b41df9a3c087620ec1dae74
cverhulle <corentin.verhulle@gmail.com>
2025-08-14 10:36:56 +0200

    Test de la couleur des objets : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53af0d92ad1e06d7f9a7fa55f284bc2e23f717d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d0a6a850af017635097ec427f009790d5756f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
