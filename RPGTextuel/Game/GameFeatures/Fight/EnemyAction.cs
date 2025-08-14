638c6a7055325cf996a9a255e522d2f90fdfd884
cverhulle <corentin.verhulle@gmail.com>
2025-08-14 11:03:44 +0200

    En fonction du combat de boss ou non, on gère l'affichage !

--- RPGTextuel/Game/GameFeatures/Fight/EnemyAction.cs
+++ RPGTextuel/Game/GameFeatures/Fight/EnemyAction.cs
@@ -1,3 +1,4 @@
+using RPGTextuel.Config;
 using RPGTextuel.Core;
 using RPGTextuel.Enemies.Class;
 using RPGTextuel.Game.GameUtilsNamespace;
@@ -10,6 +11,9 @@ namespace RPGTextuel.Game.GameFeatures.Fight
         // Cette méthode gère le tour des ennemis.
         public static void EnemyTurn(Player player, Enemy enemy, bool isFinalBoss)
         {
+            // On calcule la couleur de l'affichage
+            ConsoleColor colorToDisplay = isFinalBoss ? GameConfig.bossColor : GameConfig.ennemiesColor;
+
             // Message d'action
             Console.WriteLine("\n== TOUR DE L'ADVERSAIRE ====");
 
@@ -18,8 +22,11 @@ namespace RPGTextuel.Game.GameFeatures.Fight
 
             Console.WriteLine("============================");
 
+            // On reset la couleur de l'écran
+            Console.ResetColor();            
+
             //Pause pour que le joueur ait le temps de lire
-            GameUtils.WaitForUser();
+            GameUtils.WaitForUser(color : colorToDisplay);
         }
     }
 }
