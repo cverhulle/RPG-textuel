ea0aa7bd068bed3d0a66e8d3bd16d8d987b7f053
cverhulle <corentin.verhulle@gmail.com>
2025-08-14 11:33:29 +0200

    Test du jeu : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb1d26d1a03fe034b453ecccb52d50a8567dc36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c9effc35967c8dee46c7e946b16907e39f3468")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
