d5b3eda1cb19e3725ee1853079016482b5710696
cverhulle <corentin.verhulle@gmail.com>
2025-08-18 15:12:44 +0200

    Ajustement de l'initialisation du joueur Commentaires mis à jour ; On ne donne plus de potion de dégats moyenne

--- RPGTextuel/Game/GameFeatures/InitGame/InitPlayerCreation.cs
+++ RPGTextuel/Game/GameFeatures/InitGame/InitPlayerCreation.cs
@@ -12,13 +12,12 @@ namespace RPGTextuel.Game.GameFeatures.InitGame
             // On instance un élément de type Player.
             Player player = new Player(playerName);
 
-            // On donne une potion de soin petite et une autre moyenne au joueur
+            // On donne une petite potion de soin et une autre de taille moyenne au joueur
             player.AddHealPotion(PotionSize.Small);
             player.AddHealPotion(PotionSize.Medium);
 
-            // On donne une potion de dégats petite et une autre moyenne au joueur
+            // On donne une petite potion de dégats au joueur
             player.AddDamagePotion(PotionSize.Small);
-            player.AddDamagePotion(PotionSize.Medium);
 
             // On retourne le joueur crée.
             return player;
