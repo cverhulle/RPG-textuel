5211ab5de4baa0fe0b0d1615f4c4865f3ab4a891
cverhulle <corentin.verhulle@gmail.com>
2025-08-18 15:06:23 +0200

    On gère la couleur de l'affichage.

--- RPGTextuel/Game/GameFeatures/Fight/EventAfterFight.cs
+++ RPGTextuel/Game/GameFeatures/Fight/EventAfterFight.cs
@@ -1,3 +1,4 @@
+using RPGTextuel.Config;
 using RPGTextuel.Config.Enum;
 using RPGTextuel.Core;
 using RPGTextuel.RandomEvent.Factory;
@@ -10,8 +11,17 @@ namespace RPGTextuel.Game.GameFeatures.Fight
         // Cette méthode permet de faire gagner un objet au joueur après un combat en utilisant la table par défaut.
         public static void EarnAnItem(Player player)
         {
+            // On affiche le texte en couleur
+            Console.ForegroundColor = GameConfig.eventsColor;
+
+            // On récupère l'événement à lancer
             var itemToEarn = RandomEventFactory.GetRandomItem(ItemDropTableType.Default);
+
+            // On décleche l'événement (ajout de l'objet dans l'inventaire et affichage)
             itemToEarn.Trigger(player);
+
+            // On remet la couleur par défaut
+            Console.ForegroundColor = GameConfig.defaultColor;
         }
     }
 }
