03cd53973de1cc2a6c5c6f0a9b5752d832d1012a
cverhulle <corentin.verhulle@gmail.com>
2025-08-18 15:04:54 +0200

    Test : La couleur du texte n'est pas bonne. L'implémentation est correcte.

--- RPGTextuel/Test/TestLauncher.cs
+++ RPGTextuel/Test/TestLauncher.cs
@@ -1,4 +1,4 @@
-using RPGTextuel.Test.CoreCharacter;
+using RPGTextuel.Test.Game.TestGameFeatures;
 
 namespace RPGTextuel.Test
 {
@@ -8,7 +8,7 @@ namespace RPGTextuel.Test
         // On lance cette méthode pour tester le programme.
         public static void TestLauncherMain()
         {
-            TestCharacterCombat.TestAttackWithCriticalHit();
+            TestEventAfterAFight.TestEarnAnItem();
         }
     }
 }
