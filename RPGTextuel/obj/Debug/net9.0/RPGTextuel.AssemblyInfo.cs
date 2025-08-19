1de6a8a2dba573be903305f9000532c1426e30f8
cverhulle <corentin.verhulle@gmail.com>
2025-08-19 10:37:35 +0200

    Test : On aimerait garder la force en nombre entier.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744641e75d4d2190b9d9fddf9e363a49d8a30114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c49ac3b0ba02bdbaf2c39493398a720c26a910")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
