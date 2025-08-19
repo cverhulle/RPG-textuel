99bb57555b9a4997d4e2cd511b172414459d7556
cverhulle <corentin.verhulle@gmail.com>
2025-08-19 10:16:46 +0200

    Implémentation du Trigger.

--- RPGTextuel/RandomEvent/SetupEvent/GainStrengthPlayer.cs
+++ RPGTextuel/RandomEvent/SetupEvent/GainStrengthPlayer.cs
@@ -1,4 +1,5 @@
 using RPGTextuel.Core;
+using RPGTextuel.Core.PlayerNamespace;
 using RPGTextuel.RandomEvent.Class;
 
 namespace RPGTextuel.RandomEvent.SetupEvent
@@ -14,6 +15,18 @@ namespace RPGTextuel.RandomEvent.SetupEvent
         // Gain aléatoire entre 1 et 3 points de Force
         public override void Trigger(Player player)
         {
+            // On applique la méthode générique
+            player.Strength = (int)PlayerStatHelper.ModifyStatWithBounds(   
+                stat: player.Strength,                                              // stat actuelle
+                min: 1,                                                             // valeur minimale possible
+                max: 999,                                                           // valeur max qu’on s’autorise
+                randomMinChange: 1,                                                 // gain minimal (ici en "points bruts")
+                randomMaxChange: 3,                                                 // gain maximal
+                increase: true,                                                     // on augmente
+                alreadyAtBoundMessage: "Votre force est déjà au maximum 💪",
+                changeMessageTemplate: "Vous gagnez {0} points de force !",         // ici on n'est pas en %, donc {0} directement
+                finalValueMessageTemplate: "Votre force est maintenant de {0}."     // pareil
+            );
         }
     }
 }
