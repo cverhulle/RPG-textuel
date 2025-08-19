c7e190f990fdff7bfa1d19181646ac6d44519e36
cverhulle <corentin.verhulle@gmail.com>
2025-08-19 10:40:22 +0200

    Test des méthodes de gain de force : OK (attention faute quand c'est 1 point)

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c49ac3b0ba02bdbaf2c39493398a720c26a910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8e38ffd7d647a7f4b98010e47174d6630a4173")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
