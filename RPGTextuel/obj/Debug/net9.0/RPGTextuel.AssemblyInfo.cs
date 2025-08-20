8f74a3281ae79abfc1ee8a09cb391d3742e5bad0
cverhulle <corentin.verhulle@gmail.com>
2025-08-20 16:19:06 +0200

    Test du jeu A corriger : Tous les coeurs sont gris si le joueur à moins de 10 PV

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2c195f2f94d9198cffde4d25281554e636c9e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a03c50dacd431f22713ff34cb391e642f034537")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
