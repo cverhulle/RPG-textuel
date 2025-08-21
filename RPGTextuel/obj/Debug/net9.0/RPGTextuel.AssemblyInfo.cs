51e93fa105d0ffceea1da72e295c7b469f8e2a16
cverhulle <corentin.verhulle@gmail.com>
2025-08-21 11:57:09 +0200

    Test du jeu : Tous les bugs connus sont résolus.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0097c61189ed7357375d99630108317d93388f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d60cdaf874b8ec659be4a442f2ecc50cec5d5eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
