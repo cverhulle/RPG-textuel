d9e305a175350bb7eb5406d13687a31cba42c0a4
cverhulle <corentin.verhulle@gmail.com>
2025-08-21 11:13:25 +0200

    On modifie la ligne 53 "filled =" dans  GetFilledAndEmptyHeart

--- RPGTextuel/Extensions/Character/CharacterExtensions.cs
+++ RPGTextuel/Extensions/Character/CharacterExtensions.cs
@@ -50,7 +50,9 @@ namespace RPGTextuel.Extensions.Characters
             ratio = Math.Clamp(ratio, 0, 1); // On fait en sorte que ratio reste entre 0 et 1.
 
             // On calcule le nombre de coeurs de vie on doit afficher.
-            int filled = (int)(ratio * total);
+                int filled = character.Health > 0 
+                    ? Math.Max(1, (int)(ratio * total)) 
+                    : 0;
 
             // On calcule le nombre de coeurs vides à afficher
             int empty = total - filled;
