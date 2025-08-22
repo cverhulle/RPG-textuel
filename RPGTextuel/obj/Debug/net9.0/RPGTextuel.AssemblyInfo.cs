75630814f45bf4e2ea91dfab98f00f059ff90831
cverhulle <corentin.verhulle@gmail.com>
2025-08-22 11:59:08 +0200

    On déplace PlayerStatHelper vers le dossier Utils et, on le renomme.

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ecd55ba81a158cd7772e707565dd3b38584062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec472e2ded30e506ddc54c838948c61c28110cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
