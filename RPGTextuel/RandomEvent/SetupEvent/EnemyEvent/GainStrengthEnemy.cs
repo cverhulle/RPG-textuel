b8f9d24c1b64b6a0fc2d67256dd21310fd1acfd8
cverhulle <corentin.verhulle@gmail.com>
2025-08-22 11:57:54 +0200

    Implémentation de l'événement.

--- RPGTextuel/RandomEvent/SetupEvent/EnemyEvent/GainStrengthEnemy.cs
+++ RPGTextuel/RandomEvent/SetupEvent/EnemyEvent/GainStrengthEnemy.cs
@@ -1,3 +1,4 @@
+using RPGTextuel.Core.PlayerNamespace;
 using RPGTextuel.Enemies.Class;
 using RPGTextuel.RandomEvent.Class;
 
@@ -13,6 +14,19 @@ namespace RPGTextuel.RandomEvent.SetupEvent
         // On définit l'activation de l'événement.
         public override void Trigger(Enemy enemy)
         {
+            enemy.Strength = (int)PlayerStatHelper.ModifyStatWithBounds(
+                stat: enemy.Strength,                                                                   // Stat à modifier
+                min: 1,                                                                                 // Minimum 1 de force
+                max: 999,                                                                               // Maximum 999 de force
+                randomMinChange: 1,                                                                     // Au minimum on gagne 1
+                randomMaxChange: 2,                                                                     // Au maximum, on gagne 2 de force
+                increase: true,                                                                         // C'est une augmentation
+                alreadyAtBoundMessage: "Le prochain adversaire est déjà au sommet de son art !",        // Message si l'ennemi est déjà à 999 de force.
+                changeMessageTemplate: "La force du prochain adversaire a augmenté de {0:0.#}% !",      
+                finalValueMessageTemplate: "Force finale du prochain adversaire : {0:0.#}%",           // Message pour l'utilisateur
+                isPercentage: false,                                                                    // Les valeurs ne sont pas en pourcentage
+                roundToInt: true                                                                        // La force est un entier
+            );
         }
     }
 }
