07d6f6df32cb9051710be82db615a640b082ea5b
cverhulle <corentin.verhulle@gmail.com>
2025-08-25 11:11:22 +0200

    Implémentation de GainStrengthEnemyUtils

---
+++ RPGTextuel/Test/RandomEvent/SetupEvent/EnemyEvent/EnemyRandomEventsTestsUtils.cs
@@ -0,0 +1,18 @@
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.RandomEvent.SetupEvent;
+
+namespace RPGTextuel.Test.RandomEvents
+{
+    // Cette classe regroupe les méthodes d'aide liées aux évenements aléatoires des ennemis.
+    public static class EnemyRandomEventsTestsUtils
+    {
+        // Méthode utilitaire liée à l'évenement GainStrengthEnemy avec les items par défaut.
+        public static void GainStrengthEnemyUtils(Enemy enemy)
+        {
+            Console.WriteLine($"Force initiale : {enemy.Strength}");
+
+            GainStrengthEnemyEvent evt = new GainStrengthEnemyEvent();
+            evt.Trigger(enemy);
+        }
+    }
+}
--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34682a732288411027f0c0161f70441390488e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319da11817ccbcdf7019b6846a60fd362450eabe")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
