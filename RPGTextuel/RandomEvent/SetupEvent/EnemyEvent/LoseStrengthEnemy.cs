af27bd4f4c4ef47543d9377c889bcbc75f5b0674
cverhulle <corentin.verhulle@gmail.com>
2025-08-26 11:00:38 +0200

    Début de l'implémentation de LoseStrengthEnemyEvent

---
+++ RPGTextuel/RandomEvent/SetupEvent/EnemyEvent/LoseStrengthEnemy.cs
@@ -0,0 +1,31 @@
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.RandomEvent.Class;
+using RPGTextuel.Utils;
+namespace RPGTextuel.RandomEvent.SetupEvent
+{
+    // On définit l'événement "Gain de force pour le prochain ennemi
+    public class LoseStrengthEnemyEvent : EnemyRandomEventClass
+    {
+        // On définit son nom et sa description.
+        public override string Name => "Un adversaire impressionnant";
+        public override string Description => "D'après les rumeurs, le prochain adversaire est puissant...";
+
+        // On définit l'activation de l'événement.
+        public override void Trigger(Enemy enemy)
+        {
+            enemy.Strength = (int)ModifyStatHelper.ModifyStatWithBounds(
+                stat: enemy.Strength,                                                                   // Stat à modifier
+                min: 1,                                                                                 // Minimum 1 de force
+                max: 999,                                                                               // Maximum 999 de force
+                randomMinChange: 1,                                                                     // Au minimum on gagne 1
+                randomMaxChange: 2,                                                                     // Au maximum, on gagne 2 de force
+                increase: true,                                                                         // C'est une augmentation
+                alreadyAtBoundMessage: "Le prochain adversaire est déjà au sommet de son art !",        // Message si l'ennemi est déjà à 999 de force.
+                changeMessageTemplate: "La force du prochain adversaire a augmenté de {0:0.#} !",      // Message pour l'utilisateur
+                finalValueMessageTemplate: "Force finale du prochain adversaire : {0:0.#}",            // Message pour l'utilisateur
+                isPercentage: false,                                                                    // Les valeurs ne sont pas en pourcentage
+                roundToInt: true                                                                        // La force est un entier
+            );
+        }
+    }
+}
--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41760994f14915fd3ccc9550867b03f5598db21b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2839322bdd7eede3cd4922a81ad00e0c6633d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
