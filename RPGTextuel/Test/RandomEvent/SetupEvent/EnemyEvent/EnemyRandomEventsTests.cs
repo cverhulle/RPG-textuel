91057bf91d389b22b2d0ee900139648e0c5f3032
cverhulle <corentin.verhulle@gmail.com>
2025-08-26 11:06:02 +0200

    Implémentation de TestLoseStrengthEnemyEventByDefault

--- RPGTextuel/Test/RandomEvent/SetupEvent/EnemyEvent/EnemyRandomEventsTests.cs
+++ RPGTextuel/Test/RandomEvent/SetupEvent/EnemyEvent/EnemyRandomEventsTests.cs
@@ -47,5 +47,48 @@ namespace RPGTextuel.Test.RandomEvents
 
             GameUtils.WaitForUser();
         }
+
+        // Test de LoseStrengthEnemyEvent par défaut
+        public static void TestLoseStrengthEnemyEventByDefault()
+        {
+            Console.WriteLine("===== Test de LoseStrengthEnemyEventByDefault =====");
+
+            Enemy enemy = EnemyTestFactory.CreateGoblin();
+
+            Console.WriteLine("Résultat attendu : Diminution aléatoire de la force (2 à 4).");
+            EnemyRandomEventsTestsUtils.LoseStrengthEnemyUtils(enemy);
+
+            GameUtils.WaitForUser();
+        }
+
+        /*
+        // Test de GainStrengthEnemyEvent avec force proche du maximum
+        public static void TestGainStrengthEnemyEventWithAlmostMaxStrength()
+        {
+            Console.WriteLine("===== Test de GainStrengthEnemyEventWithAlmostMaxStrength =====");
+
+            Enemy enemy = EnemyTestFactory.CreateGoblin();
+
+            enemy.Strength = 998;
+            Console.WriteLine("Résultat attendu : Gain limité (1 point max).");
+            EnemyRandomEventsTestsUtils.GainStrengthEnemyUtils(enemy);
+
+            GameUtils.WaitForUser();
+        }
+
+        // Test de GainStrengthEnemyEvent avec force déjà au maximum
+        public static void TestGainStrengthEnemyEventWithMaxStrength()
+        {
+            Console.WriteLine("===== Test de GainStrengthEventWithMaxStrength =====");
+
+            Enemy enemy = EnemyTestFactory.CreateGoblin();
+
+            enemy.Strength = 999;
+            Console.WriteLine("Résultat attendu : Message indiquant que la force est déjà au maximum.");
+            EnemyRandomEventsTestsUtils.GainStrengthEnemyUtils(enemy);
+
+            GameUtils.WaitForUser();
+        }
+        */
     }
 }
