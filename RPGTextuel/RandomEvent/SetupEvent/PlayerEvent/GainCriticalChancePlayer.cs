c0c77db7e9c36773003ab3269effd99ba9e70e2a
cverhulle <corentin.verhulle@gmail.com>
2025-09-01 16:22:37 +0200

    Implémentation de l'interface.

--- RPGTextuel/RandomEvent/SetupEvent/PlayerEvent/GainCriticalChancePlayer.cs
+++ RPGTextuel/RandomEvent/SetupEvent/PlayerEvent/GainCriticalChancePlayer.cs
@@ -1,19 +1,23 @@
 using RPGTextuel.Core;
+using RPGTextuel.Enemies.Class;
 using RPGTextuel.RandomEvent.Class;
+using RPGTextuel.RandomEvent.Class.Interface;
 using RPGTextuel.Utils;
 
 namespace RPGTextuel.RandomEvent.SetupEvent
 {
     // On définit l'événement "Augmentation de la chance de critique".
-    public class GainCriticalChancePlayerEvent : PlayerRandomEventClass
+    public class GainCriticalChancePlayerEvent : IRandomEvent
     {
         // On définit son nom et sa description.
-        public override string Name => "Eclair de génie !";
-        public override string Description => "Un éclair de lucidité traverse votre esprit. Vos prochaines attaques seront plus précises !";
+        public string Name => "Eclair de génie !";
+        public string Description => "Un éclair de lucidité traverse votre esprit. Vos prochaines attaques seront plus précises !";
 
         // On définit l'effet de l'événement : Augmentation de la chance de coup critique.
-        public override void Trigger(Player player)
+        public void Trigger(Player? player, Enemy? enemy)
         {
+            if (player == null) return; // Sécurité
+
             player.CriticalHitChance = ModifyStatHelper.ModifyStatWithBounds
                 (
                     player.CriticalHitChance,                                       // Stat à modifier (chance de coup critique)
