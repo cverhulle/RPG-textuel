65f8fd13a73b6ccc7b35ab620f63cfe166058ffc
cverhulle <corentin.verhulle@gmail.com>
2025-09-01 16:30:10 +0200

    Implémentation de l'interface.

--- RPGTextuel/RandomEvent/SetupEvent/PlayerEvent/PeacefulDayEvent.cs
+++ RPGTextuel/RandomEvent/SetupEvent/PlayerEvent/PeacefulDayEvent.cs
@@ -1,17 +1,20 @@
 using RPGTextuel.Core;
-using RPGTextuel.RandomEvent.Class;
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.RandomEvent.Class.Interface;
 
 namespace RPGTextuel.RandomEvent.SetupEvent
 {
     // On définit l'évenement "Jour de paix" : il ne "fait rien"
-    public class PeacefulDayEvent : PlayerRandomEventClass
+    public class PeacefulDayEvent : IRandomEvent
     {
         // On définit le nom et la description de l'événement.
-        public override string Name => "Jour paisible";
-        public override string Description => "Le soleil est radieux, le vent est doux...";
+        public string Name => "Jour paisible";
+        public string Description => "Le soleil est radieux, le vent est doux...";
 
-        public override void Trigger(Player player)
+        public void Trigger(Player? player, Enemy? enemy)
         {
+            if (player == null) return; // Sécurité
+
             // Il ne se passe rien, juste un message d'information.
             Console.WriteLine("... Une journée paisible sans incident.");
         }
