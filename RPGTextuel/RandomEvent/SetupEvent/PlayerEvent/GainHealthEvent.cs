02d5f3b2fed8e232eb7b0bd6fe04b70a7b8fc001
cverhulle <corentin.verhulle@gmail.com>
2025-09-01 16:23:21 +0200

    Implémentation de l'interface.

--- RPGTextuel/RandomEvent/SetupEvent/PlayerEvent/GainHealthEvent.cs
+++ RPGTextuel/RandomEvent/SetupEvent/PlayerEvent/GainHealthEvent.cs
@@ -1,19 +1,22 @@
 using RPGTextuel.Core;
-using RPGTextuel.RandomEvent.Class;
+using RPGTextuel.Enemies.Class;
+using RPGTextuel.RandomEvent.Class.Interface;
 
 namespace RPGTextuel.RandomEvent.SetupEvent
 {
     // On définit l'évenement "Gain de Points de vie".
-    public class GainHealthEvent : PlayerRandomEventClass
+    public class GainHealthEvent : IRandomEvent
     {
         // On définit son nom et sa description.
-        public override string Name => "Délicieux repas";
-        public override string Description => "Ce délicieux repas vous fait regagner des points de vie !";
+        public string Name => "Délicieux repas";
+        public string Description => "Ce délicieux repas vous fait regagner des points de vie !";
 
         // On définit son activation : 
         // Gain aléatoire entre 10 et 30 PV.
-        public override void Trigger(Player player)
+        public void Trigger(Player? player, Enemy? enemy)
         {
+            if (player == null) return; // Sécurité
+
             // On choisit aléatoirement le montant de PV gagné.
             int healAmount = Random.Shared.Next(10, 30);
 
