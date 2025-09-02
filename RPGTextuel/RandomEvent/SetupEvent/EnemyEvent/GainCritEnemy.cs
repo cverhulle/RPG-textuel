e3145d0da82f98cbd3cedf4f9a67bda7980805c5
cverhulle <corentin.verhulle@gmail.com>
2025-09-02 14:53:17 +0200

    Implémentation du trigger.

--- RPGTextuel/RandomEvent/SetupEvent/EnemyEvent/GainCritEnemy.cs
+++ RPGTextuel/RandomEvent/SetupEvent/EnemyEvent/GainCritEnemy.cs
@@ -14,6 +14,21 @@ namespace RPGTextuel.RandomEvent.SetupEvent
         // On définit l'activation de l'événement.
         public void Trigger(Player? player = null, Enemy? enemy = null)
         {
+            if (enemy == null) return; // Sécurité
+
+            enemy.CriticalHitChance = (int)ModifyStatHelper.ModifyStatWithBounds(
+                stat: enemy.CriticalHitChance,                                                                   // Stat à modifier
+                min: 0.0,                                                                                 // Minimum 1 de force
+                max: 1.0,                                                                               // Maximum 999 de force
+                randomMinChange: 0.05,                                                                     // Au minimum on gagne 1
+                randomMaxChange: 0.15,                                                                     // Au maximum, on gagne 2 de force
+                increase: true,                                                                         // C'est une augmentation
+                alreadyAtBoundMessage: "Le prochain adversaire est déjà expert en critique !",        // Message si l'ennemi est déjà à 999 de force.
+                changeMessageTemplate: "La chance de coup critique du prochain adversaire a augmenté de {0:0.#}% !",      // Message pour l'utilisateur
+                finalValueMessageTemplate: "Chance critique finale : {0:0.#}%",            // Message pour l'utilisateur
+                isPercentage: true,                                                                    // Les valeurs ne sont pas en pourcentage
+                roundToInt: false                                                                        // La force est un entier
+            );
         }
     }
 }
