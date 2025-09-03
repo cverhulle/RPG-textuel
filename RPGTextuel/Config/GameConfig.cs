d021195183d4dd5760c7993138f6c4f7fac6fabc
cverhulle <corentin.verhulle@gmail.com>
2025-09-03 11:34:16 +0200

    On supprime les critiques du fichier.

--- RPGTextuel/Config/GameConfig.cs
+++ RPGTextuel/Config/GameConfig.cs
@@ -15,18 +15,5 @@ namespace RPGTextuel.Config
         // Paramètres de jeu 
         // Taille Maximum pour le nom des personnages.
         public const int MaxNameLength = 15;
-
-        // Coup critique 
-        // Chance de coup critique par défaut
-        public const double criticalHitByDefault = 0.05;
-
-        // Chance de coup critique légérement augmenté
-        public const double criticalHitAugmented = criticalHitByDefault + 0.1;
-
-        // Chance de coup critique pour le joueur
-        public const double criticalHitPlayer = 0.15;
-
-        // Multiplicateur pour des dégats en coup critique
-        public const double criticalMultiplicator = 1.4;
     }
 } 
