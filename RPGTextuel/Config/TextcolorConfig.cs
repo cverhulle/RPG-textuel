d90e53a61fc99081a794a3f52bf1530474c0271e
cverhulle <corentin.verhulle@gmail.com>
2025-09-03 11:22:42 +0200

    On déplace les couleurs dans le fichier dédié.

--- RPGTextuel/Config/TextcolorConfig.cs
+++ RPGTextuel/Config/TextcolorConfig.cs
@@ -3,5 +3,35 @@ namespace RPGTextuel.Config
     // Cette classe s'occupe des couleurs des textes.
     public static class TextColorConfig
     {
+        // Couleur des textes
+        // Couleur des ennemis
+        public const ConsoleColor ennemiesColor = ConsoleColor.DarkRed;
+
+        // Couleur des boss
+        public const ConsoleColor bossColor = ConsoleColor.Red;
+
+        // Couleur des coups critique 
+        public const ConsoleColor critColor = ConsoleColor.Yellow;
+
+        // Couleur de la défaite
+        public const ConsoleColor defeatColor = ConsoleColor.DarkRed;
+
+        // Couleur par défaut
+        public const ConsoleColor defaultColor = ConsoleColor.Black;
+
+        // Couleur des événements
+        public const ConsoleColor eventsColor = ConsoleColor.Blue;
+
+        // Couleur des objets
+        public const ConsoleColor itemsColor = ConsoleColor.DarkMagenta;
+
+        // Couleur de l'intro
+        public const ConsoleColor introColor = ConsoleColor.Cyan;
+
+        // Couleur pour le joueur
+        public const ConsoleColor playerColor = ConsoleColor.Green;
+
+        // Couleur de la victoire
+        public const ConsoleColor victoryColor = ConsoleColor.DarkGreen;
     }
 }
