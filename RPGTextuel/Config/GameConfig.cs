989a8506c84e01fe00db2ccbfd93a4ca460a9fbf
cverhulle <corentin.verhulle@gmail.com>
2025-09-03 11:28:21 +0200

    On supprime les couleurs de GameConfig.

--- RPGTextuel/Config/GameConfig.cs
+++ RPGTextuel/Config/GameConfig.cs
@@ -28,36 +28,5 @@ namespace RPGTextuel.Config
 
         // Multiplicateur pour des dégats en coup critique
         public const double criticalMultiplicator = 1.4;
-
-        // Couleur des textes
-        // Couleur des ennemis
-        public const ConsoleColor ennemiesColor = ConsoleColor.DarkRed;
-
-        // Couleur des boss
-        public const ConsoleColor bossColor = ConsoleColor.Red;
-
-        // Couleur des coups critique 
-        public const ConsoleColor critColor = ConsoleColor.Yellow;
-
-        // Couleur de la défaite
-        public const ConsoleColor defeatColor = ConsoleColor.DarkRed;
-
-        // Couleur par défaut
-        public const ConsoleColor defaultColor = ConsoleColor.Black;
-
-        // Couleur des événements
-        public const ConsoleColor eventsColor = ConsoleColor.Blue;
-
-        // Couleur des objets
-        public const ConsoleColor itemsColor = ConsoleColor.DarkMagenta;
-
-        // Couleur de l'intro
-        public const ConsoleColor introColor = ConsoleColor.Cyan;
-
-        // Couleur pour le joueur
-        public const ConsoleColor playerColor = ConsoleColor.Green;
-
-        // Couleur de la victoire
-        public const ConsoleColor victoryColor = ConsoleColor.DarkGreen;
     }
 } 
