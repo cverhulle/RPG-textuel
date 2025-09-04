c0f179f0f04608a3079d2d1d845bfb9c1d851ef9
cverhulle <corentin.verhulle@gmail.com>
2025-09-04 10:59:44 +0200

    On ajoute la boucle while pour rejouer.

--- RPGTextuel/Game/Game.cs
+++ RPGTextuel/Game/Game.cs
@@ -9,24 +9,31 @@ namespace RPGTextuel.Game
 {
     // Cette classe s'occupe de gérer la boucle de jeu.
     public static class Game
-    {
+    {        
         // Cette méthode a pour but de lancer la partie
         public static void StartGame()
         {
-            // On initialise la couleur par défaut
-            Console.ForegroundColor = TextColorConfig.defaultColor;
-            
-            // On initialise la partie
-            (Player player, List<Enemy> ennemies) = InitGame.InitTheGame();
+            // On regarde si le joueur souhaite rejouer.
+            bool replay = true;
 
-            // On lance la boucle de jeu
-            // En cas d'arret de la partie, on récupère le booléan "true".
-            Boolean stopGame = MainMenu.HandleMainMenu(player, ennemies);
-
-            // Si tous les ennemis sont vaincus, on affiche le message de victoire.
-            if (!stopGame)
+            // Tant que l'on rejoue, la partie continue
+            while (replay)
             {
-                EndGame.VictoryMessage();
+                // On initialise la couleur par défaut
+                Console.ForegroundColor = TextColorConfig.defaultColor;
+
+                // On initialise la partie
+                (Player player, List<Enemy> ennemies) = InitGame.InitTheGame();
+
+                // On lance la boucle de jeu
+                // En cas d'arret de la partie, on récupère le booléan "true".
+                Boolean stopGame = MainMenu.HandleMainMenu(player, ennemies);
+
+                // Si tous les ennemis sont vaincus, on affiche le message de victoire.
+                if (!stopGame)
+                {
+                    EndGame.VictoryMessage();
+                }
             }
         }
     }
