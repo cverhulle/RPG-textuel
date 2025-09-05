8f74dadafe69608a776af97c6c65686232ddd1e0
cverhulle <corentin.verhulle@gmail.com>
2025-09-05 11:12:20 +0200

    On crée les messages d'accueil  dans FirstRunMessages et ReplayMessages

--- RPGTextuel/Game/GameFeatures/InitGame/WelcomeMessage.cs
+++ RPGTextuel/Game/GameFeatures/InitGame/WelcomeMessage.cs
@@ -3,6 +3,26 @@ namespace RPGTextuel.Game.GameFeatures.InitGame
     // Cette classe s'occupe de gérer l'affichage pour le jeu.
     public static class InitWelcomeMessage
     {
+        // Méthode random pour choisir un message aléatoire
+        private static readonly Random rnd = new Random();
+
+        // Messages pour la première partie.
+        private static readonly string[] FirstRunMessages =
+        {
+            "Vous allez devoir affronter trois ennemis redoutables...",
+            "Votre courage sera mis à rude épreuve face à vos adversaires...",
+            "Trois épreuves mortelles se dressent devant vous..."
+        };
+
+        // Messages pour les parties où l'on rejoue.
+        private static readonly string[] ReplayMessages =
+        {
+            "Un nouveau défi vous attend, {0} !",
+            "Les ténèbres reviennent, {0}… il est temps de reprendre les armes.",
+            "{0}, votre légende n'est pas encore écrite en entier !",
+            "Encore une aventure épique pour vous, {0} !"
+        };
+
         // Cette méthode affiche le message de début de jeu
         public static void ShowWelcomeMessage()
         {
