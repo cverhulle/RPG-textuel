19e66efb820fbf10499c512e2be72c6189adb5d4
cverhulle <corentin.verhulle@gmail.com>
2025-09-05 10:16:39 +0200

    On supprime InitTheGame et, on ajuste les commentaires.

--- RPGTextuel/Game/GameFeatures/InitGame/InitGame.cs
+++ RPGTextuel/Game/GameFeatures/InitGame/InitGame.cs
@@ -7,41 +7,9 @@ namespace RPGTextuel.Game.GameFeatures.InitGame
 {
     public static class InitGame
     {
-        // Cette méthode permet d'initialiser la partie.
-        // Elle affiche les messages d'accueils, demande le nom du joueur et l'instancie.
-        // Elle s'occupe également de créer la liste des ennemis à affronter pendant cette partie.
-        // Elle retourne le joueur ainsi crée de type Player et la liste des ennemis.
-        public static (Player player, List<Enemy> ennemies) InitTheGame()
-        {
-            // On règle la couleur des textes.
-            Console.ForegroundColor = TextColorConfig.introColor;
-
-            // On affiche le message d'accueil
-            InitWelcomeMessage.ShowWelcomeMessage();
-
-            // On récupère le nom du joueur
-            string name = InitPlayerName.AskAndVerifyPlayerName();
-
-            // On crée l'instance de type joueur
-            Player player = InitPlayerCreation.CreateAPlayer(name);
-
-            // On crée la liste des ennemis pendant cette partie
-            List<Enemy> ennemies = InitEnnemiesToFight.GetRandomGoblinSet();
-
-            // On affiche le dexième message d'accueil
-            InitWelcomeMessage.ShowWelcomeMessage2(name);
-
-            // On attend que l'utilisateur ait tout lu.
-            GameUtils.WaitForUser(color: TextColorConfig.introColor);
-
-            // On reset la couleur des textes.
-            Console.ForegroundColor = TextColorConfig.defaultColor;
-
-            return (player, ennemies);
-        }
-
         /// <summary>
         /// Instancie le joueur et génère la liste des ennemis pour la partie.
+        /// On retourne le joueur et la liste d'ennemis
         /// </summary>
         public static (Player player, List<Enemy> ennemies) InitNewRun(string playerName)
         {
@@ -57,6 +25,7 @@ namespace RPGTextuel.Game.GameFeatures.InitGame
         /// <summary>
         /// Demande le nom du joueur et affiche les messages d'accueils
         /// Cette méthode n'est déclenchée qu'au lancement du programme.
+        /// On retourne le nom du joueur
         /// </summary>
         public static string InitPlayerIdentity()
         {
