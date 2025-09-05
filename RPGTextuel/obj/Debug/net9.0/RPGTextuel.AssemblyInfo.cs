3d87baf5b6270cf6d21c5e1ec9acfe5efcf70952
cverhulle <corentin.verhulle@gmail.com>
2025-09-05 11:31:30 +0200

    On ajoute le $ sur {name}

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ea1212c39e78e6a3b549dac7687482e463945f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c58e515723c508e1e2083276a6e69044bf90a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
