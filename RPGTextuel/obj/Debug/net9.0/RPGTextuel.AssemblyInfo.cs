85ba45580ae2fc62c48b02d887ba94c6b3fd0777
cverhulle <corentin.verhulle@gmail.com>
2025-09-15 14:24:09 +0200

    Test de l'affichage de l'inventaire trié : Echec

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe4df92a1b622ac31c215520022080f8c1cf5c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49dcdfd5d96246bbed9b431ed21836c569faa0e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
