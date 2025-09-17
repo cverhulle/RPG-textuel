78643c88cdbfbd96408b9d3efcf45fdaaacd1ea4
cverhulle <corentin.verhulle@gmail.com>
2025-09-17 10:06:56 +0200

    Mise à jour de la documentation.

--- RPGTextuel/Enemies/Class/GoblinMinion.cs
+++ RPGTextuel/Enemies/Class/GoblinMinion.cs
@@ -3,9 +3,22 @@ using RPGTextuel.Enemies.EnemyTypes;
 
 namespace RPGTextuel.Enemies.Class
 {
-    // On crée un sous-type d'ennemi de type GoblinMinion
+    /// <summary>
+    /// Représente un ennemi de type Petit Gobelin.
+    /// Hérite de <see cref="Enemy"/> avec des statistiques faibles.
+    /// </summary>
     public class GoblinMinion : Enemy
     {
+        /// <summary>
+        /// Initialise une nouvelle instance de <see cref="GoblinMinion"/>.
+        /// </summary>
+        /// <param name="name">Nom du petit gobelin.</param>
+        /// <param name="health">Points de vie actuels.</param>
+        /// <param name="strength">Force d’attaque.</param>
+        /// <param name="maxHealth">Points de vie maximum.</param>
+        /// <param name="criticalHitChance">
+        /// Probabilité de coup critique (par défaut : <see cref="GameStatConfig.CriticalHitByDefault"/>).
+        /// </param>
         public GoblinMinion
         (
             string name,
@@ -18,10 +31,14 @@ namespace RPGTextuel.Enemies.Class
         {
         }
 
-        // Propriété pour afficher le type de l'ennemi.
+        /// <summary>
+        /// Retourne le type de l’ennemi sous forme lisible.
+        /// </summary>
         public override string DisplayType => "Petit Gobelin";
 
-        // On réécrit la méthode CLone.
+        /// <summary>
+        /// Crée une nouvelle instance de <see cref="GoblinMinion"/>.  
+        /// </summary>
         public override Enemy Clone()
         {
             return new GoblinMinion(Name, MaxHealth, Strength, MaxHealth, CriticalHitChance);
