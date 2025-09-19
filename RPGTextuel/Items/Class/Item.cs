3e0e8b8fe69e633de1f29420ba960a359de8f7ee
cverhulle <corentin.verhulle@gmail.com>
2025-09-19 09:40:26 +0200

    Mise à jour des commentaires.

--- RPGTextuel/Items/Class/Item.cs
+++ RPGTextuel/Items/Class/Item.cs
@@ -2,20 +2,33 @@ using RPGTextuel.Core.Characters;
 
 namespace RPGTextuel.Items.Class
 {
-    // On crée une classe abstraite appelée Item
+    /// <summary>
+    /// Classe abstraite représentant un objet générique du jeu.
+    /// Tous les objets héritant de cette classe doivent définir leur propre logique d'utilisation.
+    /// </summary>
     public abstract class Item
     {
-        // On liste ses attributs
+        /// <summary>
+        /// Nom de l'objet.
+        /// </summary>
         public string name { get; protected set; }
 
-        // On implémente son constructeur.
+        /// <summary>
+        /// Constructeur de la classe Item.
+        /// </summary>
+        /// <param name="nameItem">Le nom de l'objet.</param>
         public Item(string nameItem)
         {
             name = nameItem;
         }
 
-        // On oblige les classes dérivées à définir une méthode Use.
-        // On retourne un booléen correspondant au fait que l'objet soit utilisé ou non.
+        /// <summary>
+        /// Méthode abstraite permettant d'utiliser l'objet.
+        /// Doit être implémentée par les classes dérivées.
+        /// </summary>
+        /// <param name="user">Le personnage qui utilise l'objet.</param>
+        /// <param name="target">La cible sur laquelle l'objet est utilisé.</param>
+        /// <returns>True si l'objet a été utilisé, False sinon.</returns>
         public abstract Boolean Use(Character user, Character target);
     }
 }
