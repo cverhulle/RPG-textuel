2fa81e95fd49dbc96790fdf2db8ea3f31b97efa1
cverhulle <corentin.verhulle@gmail.com>
2025-09-19 09:35:01 +0200

    Test de rejouer un epartie : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050c241bb36743a0e2330125b47556023b8cad00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f12dfa91caed293c5339b38ab032fbf72fc570")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
