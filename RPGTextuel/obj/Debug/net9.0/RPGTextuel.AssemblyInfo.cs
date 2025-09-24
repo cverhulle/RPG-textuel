82c139081037c86a58c3bc40cc0bd408d883a453
cverhulle <corentin.verhulle@gmail.com>
2025-09-24 11:08:59 +0200

    Test de la réinitialisation des stats du joueur après une nouvelle partie : OK

--- RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
+++ RPGTextuel/obj/Debug/net9.0/RPGTextuel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b381ac6a781f7d02240db713a93eabac40a652")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460f2cda444b58a8407a1739567012d605717f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGTextuel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
